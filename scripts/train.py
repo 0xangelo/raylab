558e5838ebdebf1cb32b63a04bcaec67f89785f0
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-30 17:50:41 -0300

    feat: add custom training script
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ scripts/train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from ray.rllib.train import create_parser, run
+
+import raylab
+
+
+def main():
+    raylab.register_all_agents()
+    raylab.register_all_environments()
+    parser = create_parser()
+    args = parser.parse_args()
+    run(args, parser)
+
+
+if __name__ == "__main__":
+    main()
