c621a67aa26c8da3ca1a9b5f8b717decffdbd310
Ângelo Lovatto <angelolovatto@gmail.com>
2019-08-30 16:31:26 -0300

    chore: move logger to package
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- logger/__init__.py
+++
@@ -1,5 +0,0 @@
-from ray.tune.logger import TFLogger
-from .config_json_logger import ConfigJsonLogger
-from .flat_csv_logger import FlatCSVLogger
-
-DEFAULT_LOGGERS = (ConfigJsonLogger, FlatCSVLogger, TFLogger)
