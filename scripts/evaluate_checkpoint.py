48c1b6be1235fe8902bf117a34e5dc0805a0b3cc
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-02 16:09:24 -0300

    feat: add script to rollout from checkpoint
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ scripts/evaluate_checkpoint.py
@@ -0,0 +1,63 @@
+"""Simulate an agent from a given checkpoint in the desired environment."""
+import os
+import pickle
+from contextlib import suppress
+
+import click
+import gym
+import ray
+from ray.tune.registry import TRAINABLE_CLASS, _global_registry
+
+import raylab
+
+
+def get_agent(checkpoint, algo, env):
+    """Instatiate and restore agent class from checkpoint."""
+    config = {}
+    # Load configuration from file
+    config_dir = os.path.dirname(checkpoint)
+    config_path = os.path.join(config_dir, "params.pkl")
+    if not os.path.exists(config_path):
+        config_path = os.path.join(config_dir, "../params.pkl")
+    if not os.path.exists(config_path):
+        raise ValueError(
+            "Could not find params.pkl in either the checkpoint dir or "
+            "its parent directory."
+        )
+    with open(config_path, "rb") as file:
+        config = pickle.load(file)
+
+    agent_cls = _global_registry.get(TRAINABLE_CLASS, algo)
+    agent = agent_cls(env=env, config=config)
+    agent.restore(checkpoint)
+    return agent
+
+
+@click.command()
+@click.argument("checkpoint")
+@click.option("--algo", default=None, help="Name of the trainable class to run.")
+@click.option("--env", default=None, help="Name of the environment to interact with.")
+def main(checkpoint, algo, env):
+    """Produce rollouts."""
+    if not algo or not env:
+        return
+
+    raylab.register_all_agents()
+    raylab.register_all_environments()
+    ray.init()
+    agent = get_agent(checkpoint, algo, env)
+    env = gym.make(agent.config["env"])
+
+    obs, done, cummulative_reward = env.reset(), True, 0
+    with suppress(KeyboardInterrupt), env:
+        while True:
+            obs, rew, done, _ = env.step(agent.compute_action(obs))
+            cummulative_reward += rew
+            env.render()
+            if done:
+                print("cummulative_reward:", cummulative_reward)
+                obs, done, cummulative_reward = env.reset(), False, 0
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
