ad7c0e51ba41437a8f44850c089cee436b969865
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-04 11:16:39 -0300

    feat: implement remaining methods in NAFTorchPolicy
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/algorithms/naf/naf_policy.py
+++ raylab/algorithms/naf/naf_policy.py
@@ -3,22 +3,23 @@ import os
 import inspect
 
 import torch
-from ray.rllib.policy import Policy, TorchPolicy
-from ray.rllib.policy.policy import LEARNER_STATS_KEY
+from ray.rllib.utils import merge_dicts
+from ray.rllib.policy.policy import Policy, LEARNER_STATS_KEY
 from ray.rllib.utils.annotations import override
 
+from raylab.utils.pytorch import convert_to_tensor, get_optimizer_class
 from raylab.algorithms.naf.naf_module import NAFModule
 
 
 class NAFTorchPolicy(Policy):
-    """PyTorch policy to use with RLlib."""
+    """Normalized Advantage Function policy in Pytorch to use with RLlib."""
 
     # pylint: disable=abstract-method
 
     @override(Policy)
     def __init__(self, observation_space, action_space, config):
-        super().__init__(observation_space, action_space)
-        self.config = config
+        super().__init__(observation_space, action_space, config)
+        self.config = merge_dicts(self.get_default_config(), config)
         self.device = (
             torch.device("cuda")
             if bool(os.environ.get("CUDA_VISIBLE_DEVICES", None))
@@ -30,7 +31,7 @@ class NAFTorchPolicy(Policy):
         self.optimizer = self._make_optimizer(self.module, self.config)
 
     @override(Policy)
-    def compute_actions(  # pylint: disable=too-many-arguments
+    def compute_actions(  # pylint: disable=too-many-arguments,unused-argument
         self,
         obs_batch,
         state_batches,
@@ -40,7 +41,10 @@ class NAFTorchPolicy(Policy):
         episodes=None,
         **kwargs
     ):
-        pass
+        obs = convert_to_tensor(obs_batch, self.device)
+        logits = self.module.logits_module(obs)
+        action = self.module.action_module(logits)
+        return action.cpu().numpy(), state_batches, {}
 
     @override(Policy)
     def postprocess_trajectory(
@@ -62,23 +66,24 @@ class NAFTorchPolicy(Policy):
         self.module.load_state_dict(weights)
 
     @staticmethod
+    def get_default_config():
+        from raylab.algorithms.naf.naf import DEFAULT_CONFIG
+
+        return DEFAULT_CONFIG
+
+    @staticmethod
     def _make_module(obs_space, action_space, config):
         obs_dim = obs_space.shape[0]
-        action_dim = action_space.shape[0]
-        module = NAFModule(obs_dim, action_dim, config["model"])
+        action_low = torch.from_numpy(action_space.low).float()
+        action_high = torch.from_numpy(action_space.high).float()
+        module = NAFModule(obs_dim, action_low, action_high, config["model"])
         return module
 
     @staticmethod
     def _make_optimizer(module, config):
         optimizer = config["torch_optimizer"]
-        optimizer_options = config["torch_optimizer_options"]
         if isinstance(optimizer, str):
-            if optimizer == "Adam":
-                optimizer_cls = torch.optim.Adam
-            elif optimizer == "RMSprop":
-                optimizer_cls = torch.optim.RMSprop
-            else:
-                raise ValueError("Unsupported optimizer type '{}'.".format(optimizer))
+            optimizer_cls = get_optimizer_class(optimizer)
         elif inspect.isclass(optimizer):
             optimizer_cls = optimizer
         else:
@@ -88,5 +93,6 @@ class NAFTorchPolicy(Policy):
                 )
             )
 
+        optimizer_options = config["torch_optimizer_options"]
         optimizer = optimizer_cls(module.parameters(), **optimizer_options)
         return optimizer
