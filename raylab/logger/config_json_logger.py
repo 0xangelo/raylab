3bf1cdfe33fc4675b4461f6bddcfc161c9303850
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-12 13:25:42 -0300

    refactor(logger): update ConfigJsonLogger based on original
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/logger/config_json_logger.py
+++ raylab/logger/config_json_logger.py
@@ -1,22 +1,15 @@
-import os
-import json
-
-from ray.tune.logger import Logger, _SafeFallbackEncoder
-import ray.cloudpickle as cloudpickle
+# pylint: disable=missing-docstring
+from ray.tune.logger import JsonLogger as _JsonLogger
+from ray.rllib.utils.annotations import override
 
 
-class ConfigJsonLogger(Logger):
+class ConfigJsonLogger(_JsonLogger):
+    """Custom JsonLogger which only saves the trial configuration."""
+
+    @override(_JsonLogger)
     def _init(self):
         self.update_config(self.config)
 
+    @override(_JsonLogger)
     def on_result(self, result):
         pass
-
-    def update_config(self, config):
-        self.config = config
-        config_out = os.path.join(self.logdir, "params.json")
-        with open(config_out, "w") as file:
-            json.dump(self.config, file, cls=_SafeFallbackEncoder)
-        config_pkl = os.path.join(self.logdir, "params.pkl")
-        with open(config_pkl, "wb") as file:
-            cloudpickle.dump(self.config, file)
