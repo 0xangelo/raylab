478d7e74593c2abb3ed9e64f2a79b8d03f53dc33
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-13 16:55:25 -0300

    fix(logger): filter values before logging
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/logger/torch_tensorboard_logger.py
+++ raylab/logger/torch_tensorboard_logger.py
@@ -1,28 +1,37 @@
 # pylint: disable=missing-docstring
 from torch.utils.tensorboard import SummaryWriter
 from ray.tune.util import flatten_dict
-from ray.tune.logger import TFLogger
+from ray.tune.logger import Logger, VALID_SUMMARY_TYPES
 from ray.tune.result import TRAINING_ITERATION, TIME_TOTAL_S, TIMESTEPS_TOTAL
 from ray.rllib.utils.annotations import override
 
 
-class TorchTBLogger(TFLogger):
+class TorchTBLogger(Logger):
     """Tensorboard logger with no tensorflow dependency."""
 
-    @override(TFLogger)
+    @override(Logger)
     def _init(self):
         self._file_writer = SummaryWriter(self.logdir)
 
-    @override(TFLogger)
+    @override(Logger)
     def on_result(self, result):
         tmp = result.copy()
         for k in ["config", "pid", "timestamp", TIME_TOTAL_S, TRAINING_ITERATION]:
             if k in tmp:
-                del tmp[k]  # not useful to tf log these
+                del tmp[k]
 
-        values = flatten_dict(tmp, delimiter="/")
-        values.update({TRAINING_ITERATION: result[TRAINING_ITERATION]})
         step = result.get(TIMESTEPS_TOTAL) or result[TRAINING_ITERATION]
+        tmp.update({TRAINING_ITERATION: result[TRAINING_ITERATION]})
+        for key, val in flatten_dict(tmp).items():
+            if isinstance(val, tuple(VALID_SUMMARY_TYPES)):
+                self._file_writer.add_scalar("/".join(["ray", "tune", key]), val, step)
 
-        self._file_writer.add_scalars("ray/tune/", values, step)
         self._file_writer.flush()
+
+    @override(Logger)
+    def flush(self):
+        self._file_writer.flush()
+
+    @override(Logger)
+    def close(self):
+        self._file_writer.close()
