d1f9ec4a1e6b88a28389aded6d0cbe9bc6ebc175
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-19 20:36:51 -0300

    refactor(modules): remove redundant code through inheritance
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/modules/fully_connected.py
+++ raylab/modules/fully_connected.py
@@ -1,17 +1,15 @@
 # pylint: disable=missing-docstring
 import torch.nn as nn
-from ray.rllib.utils.annotations import override
 
 from raylab.utils.pytorch import get_activation
 
 
-class FullyConnected(nn.Module):
+class FullyConnected(nn.Sequential):
     """Neural network module that applies several fully connected modules to inputs."""
 
     __constants__ = {"in_features", "out_features"}
 
     def __init__(self, in_features, units=(), activation="ReLU", layer_norm=False):
-        super().__init__()
         self.in_features = in_features
         activation = get_activation(activation)
         units = (self.in_features,) + tuple(units)
@@ -21,9 +19,5 @@ class FullyConnected(nn.Module):
             if layer_norm:
                 modules.append(nn.LayerNorm(out_dim))
             modules.append(activation())
-        self.sequential_module = nn.Sequential(*modules)
         self.out_features = units[-1]
-
-    @override(nn.Module)
-    def forward(self, inputs):  # pylint: disable=arguments-differ
-        return self.sequential_module(inputs)
+        super().__init__(*modules)
--- raylab/modules/value_function.py
+++ raylab/modules/value_function.py
@@ -1,19 +1,9 @@
 # pylint: disable=missing-docstring
 import torch.nn as nn
-from ray.rllib.utils.annotations import override
 
 
-class ValueFunction(nn.Module):
+class ValueFunction(nn.Linear):
     """Neural network module mapping inputs to value function outputs."""
 
-    __constants__ = {"in_features", "out_features"}
-
     def __init__(self, in_features):
-        super().__init__()
-        self.in_features = in_features
-        self.out_features = 1
-        self.linear_module = nn.Linear(self.in_features, self.out_features)
-
-    @override(nn.Module)
-    def forward(self, logits):  # pylint: disable=arguments-differ
-        return self.linear_module(logits)
+        super().__init__(in_features, 1)
