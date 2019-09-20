d8d2e3825e5f7dba4f5bc57057b3ea3e90360cb9
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-20 11:19:31 -0300

    fix(svg): ensure scale_diag is positive
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/algorithms/svg/svg_module.py
+++ raylab/algorithms/svg/svg_module.py
@@ -14,13 +14,14 @@ class ParallelDynamicsModel(nn.Module):
         super().__init__()
         self.logits_modules = logits_modules
         self.loc_modules = [nn.Linear(m.out_features, 1) for m in self.logits_modules]
-        self.scale_diag = nn.Parameter(torch.zeros(len(self.logits_modules)))
+        self.log_scale = nn.Parameter(torch.zeros(len(self.logits_modules)))
 
     @override(nn.Module)
     def forward(self, obs, actions):  # pylint: disable=arguments-differ
         logits = [m(obs, actions) for m in self.logits_modules]
         loc = torch.cat([m(i) for m, i in zip(self.loc_modules, logits)], dim=-1)
-        return loc, self.scale_diag
+        scale_diag = self.log_scale.exp()
+        return loc, scale_diag
 
 
 class RecurrentPolicyModel(nn.Module):
