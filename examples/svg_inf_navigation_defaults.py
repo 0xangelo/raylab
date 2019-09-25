a3d70583caf179d8f07ab8c4aea686cca0aba1d0
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-25 15:44:06 -0300

    chore(examples): update SVG(inf) navigation defaults
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/svg_inf_navigation_defaults.py
+++ examples/svg_inf_navigation_defaults.py
@@ -17,9 +17,16 @@ def get_config():
         # Name of Pytorch optimizer class for paremetrized policy
         "on_policy_optimizer": "Adam",
         # Keyword arguments to be passed to the on-policy optimizer
-        "on_policy_optimizer_options": {"lr": 3e-4},
+        "on_policy_optimizer_options": {"lr": tune.grid_search([7e-4, 3e-4])},
         # Clip gradient norms by this value
         "max_grad_norm": 10.0,
+        # === Regularization ===
+        "kl_schedule": {
+            "initial_coeff": 0.0,
+            "desired_kl": 0.01,
+            "adaptation_coeff": 2.0,
+            "threshold": 1.5,
+        },
         # === Network ===
         # Size and activation of the fully connected networks computing the logits
         # for the policy, value function and model. No layers means the component is
@@ -51,8 +58,7 @@ def get_config():
         "sample_batch_size": 1,
         "batch_mode": "complete_episodes",
         # === Trainer ===
-        "train_batch_size": 128,
-        "timesteps_per_iteration": 1000,
+        "train_batch_size": 100,
         # === Debugging ===
         # Set the ray.rllib.* log level for the agent process and its workers.
         # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
