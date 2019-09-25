f45fa42a8d43ae0225339a93b665cc10b298f7df
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-24 21:17:42 -0300

    feat(examples): add defaults for vanilla PG on Navigation
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/pg_navigation_defaults.py
@@ -0,0 +1,32 @@
+"""Tune experiment configuration for PG on Navigation.
+
+This can be run from the command line by executing
+`python scripts/tune_experiment.py 'PG' --local-dir <experiment dir>
+    --config examples/pg_defaults.py --stop timesteps_total 1000000`
+"""
+from ray import tune
+
+
+def get_config():
+    return {
+        # === Environment ===
+        "env": "Navigation",
+        # === PG ===
+        # Learning rate
+        "lr": 0.001,
+        # Use PyTorch as backend
+        "use_pytorch": True,
+        # === RolloutWorker ===
+        "num_workers": 0,
+        "sample_batch_size": 100,
+        "batch_mode": "complete_episodes",
+        # === Model ===
+        "model": {
+            # Nonlinearity for fully connected net (tanh, relu)
+            "fcnet_activation": "tanh",
+            # Number of hidden layers for fully connected net
+            "fcnet_hiddens": [64, 64],
+            # For control envs, documented in ray.rllib.models.Model
+            "free_log_std": True,
+        },
+    }
