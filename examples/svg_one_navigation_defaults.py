8fa16a79179fbe04a875e13504896a0cadd7d423
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-25 23:42:32 -0300

    chore(examples): update SVG(1) navigation and fix typos
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/svg_one_navigation_defaults.py
+++ examples/svg_one_navigation_defaults.py
@@ -1,18 +1,18 @@
 """Tune experiment configuration for SVG(1) on Navigation.
 
 This can be run from the command line by executing
-`python scripts/tune_experiment.py 'SVG(inf)' --local-dir <experiment dir>
-    --config examples/svg_inf_defaults.py --stop timesteps_total 100000`
+`python scripts/tune_experiment.py 'SVG(1)' --local-dir <experiment dir>
+    --config examples/svg_one_navigation_defaults.py --stop timesteps_total 10000`
 """
-from ray import tune
+from ray import tune  # pylint: disable=unused-import
 
 
-def get_config():
+def get_config():  # pylint: disable=missing-docstring
     return {
         # === Environment ===
         "env": "Navigation",
         # === Replay Buffer ===
-        "buffer_size": int(1e5),
+        "buffer_size": int(1e4),
         # === Optimization ===
         # Name of Pytorch optimizer class for paremetrized policy
         "torch_optimizer": "Adam",
@@ -23,40 +23,17 @@ def get_config():
             "policy": {"lr": 1e-3},
         },
         # Clip gradient norms by this value
-        "max_grad_norm": 10.0,
+        "max_grad_norm": float("inf"),
         # === Network ===
         # Size and activation of the fully connected networks computing the logits
         # for the policy, value function and model. No layers means the component is
         # linear in states and/or actions.
-        "module": {
-            "policy": {
-                "layers": [100, 100],
-                "activation": "Tanh",
-                # "initializer": "xavier_normal",
-                "initializer": "xavier_uniform",
-                "initializer_options": {"gain": 5 / 3},
-            },
-            "value": {
-                "layers": [400, 200],
-                "activation": "Tanh",
-                # "initializer": "xavier_normal",
-                "initializer": "xavier_uniform",
-                "initializer_options": {"gain": 5 / 3},
-            },
-            "model": {
-                "layers": [40, 40],
-                "activation": "Tanh",
-                # "initializer": "xavier_normal",
-                "initializer": "xavier_uniform",
-                "initializer_options": {"gain": 5 / 3},
-            },
-        },
+        "module": {"model": {"delay_action": True}},
         # === RolloutWorker ===
         "sample_batch_size": 1,
         "batch_mode": "complete_episodes",
         # === Trainer ===
-        "train_batch_size": 128,
-        "timesteps_per_iteration": 1000,
+        "train_batch_size": 100,
         # === Debugging ===
         # Set the ray.rllib.* log level for the agent process and its workers.
         # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
