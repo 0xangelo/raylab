3e87248900337a8e2077be26225b6156a9955a8b
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-25 23:52:28 -0300

    feat(examples): add defaults for SVG(1) on CartPoleSwingUp
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/svg_one_cartpole_defaults.py
@@ -0,0 +1,44 @@
+"""Tune experiment configuration for SVG(1) on CartPoleSwingUp.
+
+This can be run from the command line by executing
+`python scripts/tune_experiment.py 'SVG(1)' --local-dir <experiment dir>
+    --config examples/svg_one_cartpole_defaults.py --stop timesteps_total 100000`
+"""
+from ray import tune  # pylint: disable=unused-import
+
+
+def get_config():  # pylint: disable=missing-docstring
+    return {
+        # === Environment ===
+        "env": "CartPoleSwingUp",
+        "env_config": {"max_episode_steps": 250, "time_aware": True},
+        # === Replay Buffer ===
+        "buffer_size": int(1e5),
+        # === Optimization ===
+        # Name of Pytorch optimizer class for paremetrized policy
+        "torch_optimizer": "Adam",
+        # Keyword arguments to be passed to the on-policy optimizer
+        "torch_optimizer_options": {
+            "model": {"lr": 1e-3},
+            "value": {"lr": 1e-3},
+            "policy": {"lr": 1e-3},
+        },
+        # Clip gradient norms by this value
+        "max_grad_norm": float("inf"),
+        # === Network ===
+        # Size and activation of the fully connected networks computing the logits
+        # for the policy, value function and model. No layers means the component is
+        # linear in states and/or actions.
+        "module": {"model": {"delay_action": True}},
+        # === RolloutWorker ===
+        "sample_batch_size": 1,
+        "batch_mode": "complete_episodes",
+        # === Trainer ===
+        "train_batch_size": 100,
+        # === Debugging ===
+        # Set the ray.rllib.* log level for the agent process and its workers.
+        # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
+        # periodically print out summaries of relevant internal dataflow (this is
+        # also printed out once at startup at the INFO level).
+        "log_level": "WARN",
+    }
