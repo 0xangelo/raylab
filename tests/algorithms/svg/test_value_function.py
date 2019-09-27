cc10df29c8426458434e4fdc2e411040c4d61ed1
Ângelo Lovatto <angelolovatto@gmail.com>
2019-09-27 16:24:55 -0300

    fix(tests): change main critic parameters before updating target
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/algorithms/svg/test_value_function.py
+++ tests/algorithms/svg/test_value_function.py
@@ -61,6 +61,8 @@ def test_target_params_update(policy_and_batch):
     )
 
     old_params = [p.clone() for p in policy.module.target_value.parameters()]
+    for param in policy.module.value.parameters():
+        param.data.add_(torch.ones_like(param))
     policy.update_targets()
     assert all(
         not torch.allclose(p, p_)
