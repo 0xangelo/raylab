3eb5f7faea16a297928493d44ea11f0feb989f8e
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-01 07:46:26 -0300

    chore(examples): showcase adaptive KL coeff for SVG on Navigation
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/svg_one_navigation_defaults.py
+++ examples/svg_one_navigation_defaults.py
@@ -21,10 +21,17 @@ def get_config():  # pylint: disable=missing-docstring
         "torch_optimizer_options": {
             "model": {"lr": 1e-3},
             "value": {"lr": 1e-3},
-            "policy": {"lr": 1e-3},
+            "policy": {"lr": 3e-4},
         },
         # Clip gradient norms by this value
         "max_grad_norm": 1e3,
+        # === Regularization ===
+        "kl_schedule": {
+            "initial_coeff": 0.01,
+            "desired_kl": 0.01,
+            "adaptation_coeff": 1.01,
+            "threshold": 1.0,
+        },
         # === Network ===
         # Size and activation of the fully connected networks computing the logits
         # for the policy, value function and model. No layers means the component is
@@ -34,13 +41,19 @@ def get_config():  # pylint: disable=missing-docstring
                 "layers": (100, 100),
                 "activation": "Tanh",
                 "input_dependent_scale": False,
+                "initializer_options": {"name": "orthogonal"},
             },
             "value": {
                 "layers": (200, 100),
                 "activation": "ELU",
-                "initializer_options": {"gain": np.sqrt(2)},
+                "initializer_options": {"name": "orthogonal", "gain": np.sqrt(2)},
             },
-            "model": {"layers": (20, 20), "activation": "Tanh", "delay_action": True},
+            "model": {
+                "layers": (20, 20),
+                "activation": "Tanh",
+                "delay_action": True,
+                "initializer_options": {"name": "orthogonal"},
+            },
         },
         # === RolloutWorker ===
         "sample_batch_size": 1,
