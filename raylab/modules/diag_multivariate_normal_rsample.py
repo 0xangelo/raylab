858ee30eaf288a2f079207c05859865e6e2c0604
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-04 20:35:18 -0300

    feat(distributions): add SquashedMultivariateNormal
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/modules/diag_multivariate_normal_rsample.py
+++ raylab/modules/diag_multivariate_normal_rsample.py
@@ -1,11 +1,9 @@
 # pylint: disable=missing-docstring
 # pylint: enable=missing-docstring
 import torch.nn as nn
-import torch.distributions as dists
 from ray.rllib.utils.annotations import override
 
-from raylab.distributions import DiagMultivariateNormal
-from raylab.distributions.transforms import TanhTransform
+from raylab.distributions import DiagMultivariateNormal, SquashedMultivariateNormal
 
 
 class DiagMultivariateNormalRSample(nn.Module):
@@ -23,19 +21,21 @@ class DiagMultivariateNormalRSample(nn.Module):
             assert not (
                 action_low is None or action_high is None
             ), "Squashing actions requires upper and lower bounds"
-            self.register_buffer("action_loc", (action_high + action_low) / 2)
-            self.register_buffer("action_scale", (action_high - action_low) / 2)
+            self.register_buffer("action_low", action_low)
+            self.register_buffer("action_high", action_high)
 
     @override(nn.Module)
     def forward(self, inputs):  # pylint: disable=arguments-differ
-        dist = DiagMultivariateNormal(
-            loc=inputs["loc"], scale_diag=inputs["scale_diag"]
-        )
         if self.squashed:
-            squash = TanhTransform(cache_size=1)
-            shift = dists.AffineTransform(
-                loc=self.action_loc, scale=self.action_scale, cache_size=1, event_dim=1
+            dist = SquashedMultivariateNormal(
+                loc=inputs["loc"],
+                scale_diag=inputs["scale_diag"],
+                low=self.action_low,
+                high=self.action_high,
             )
-            dist = dists.TransformedDistribution(dist, [squash, shift])
+        else:
+            dist = DiagMultivariateNormal(
+                loc=inputs["loc"], scale_diag=inputs["scale_diag"]
+            )
         sample = dist.mean if self.mean_only else dist.rsample()
         return sample, dist.log_prob(sample)
