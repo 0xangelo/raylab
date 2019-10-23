e53f6adc93dc6d41e9c290827d42fa3d3b123f37
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-22 21:24:54 -0300

    feat(modules): add NormalizedLinear module
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ raylab/modules/normalized_linear.py
@@ -0,0 +1,21 @@
+# pylint: disable=missing-docstring
+import torch
+import torch.nn as nn
+from ray.rllib.utils.annotations import override
+
+
+class NormalizedLinear(nn.Linear):
+    """Neural network module that enforces a norm constraint on outputs."""
+
+    __constants__ = set(nn.Linear.__constants__ + ["beta"])
+
+    def __init__(self, *args, beta, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.beta = beta
+
+    @override(nn.Linear)
+    def forward(self, inputs):  # pylint: disable=arguments-differ
+        vec = super().forward(inputs)
+        norms = vec.norm(p=1, dim=-1, keepdim=True)
+        normalized = vec * self.out_features * self.beta / norms
+        return torch.where(norms / self.out_features > self.beta, normalized, vec)
