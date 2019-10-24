4c4d20e51e74068aa7f9618ef8cc82f3bbf9cb75
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-24 19:43:00 -0300

    chore(examples): update SAC defaults on CartPoleSwingUp
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/SAC/cartpole_defaults.py
+++ examples/SAC/cartpole_defaults.py
@@ -17,6 +17,31 @@ def get_config():
         },
         # === Replay Buffer ===
         "buffer_size": int(1e5),
+        # === Optimization ===
+        # PyTorch optimizer to use for policy
+        "policy_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # PyTorch optimizer to use for critic
+        "critic_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # PyTorch optimizer to use for entropy coefficient
+        "alpha_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # === Network ===
+        # Size and activation of the fully connected networks computing the logits
+        # for the policy and action-value function. No layers means the component is
+        # linear in states and/or actions.
+        "module": {
+            "policy": {
+                "units": (128, 128),
+                "activation": "ReLU",
+                "initializer_options": {"name": "xavier_uniform"},
+                "input_dependent_scale": True,
+            },
+            "critic": {
+                "units": (128, 128),
+                "activation": "ReLU",
+                "initializer_options": {"name": "xavier_uniform"},
+                "delay_action": True,
+            },
+        },
         # === Exploration ===
         "pure_exploration_steps": 5000,
         # === Trainer ===
@@ -26,6 +51,8 @@ def get_config():
         # Evaluate with every `evaluation_interval` training iterations.
         # The evaluation stats will be reported under the "evaluation" metric key.
         "evaluation_interval": 5,
+        # Number of episodes to run per evaluation period.
+        "evaluation_num_episodes": 5,
         # === Debugging ===
         # Set the ray.rllib.* log level for the agent process and its workers.
         # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
