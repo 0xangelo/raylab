fa8637e6c535035bdb1456ceb494d533e99932eb
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-27 23:18:00 -0300

    feat(examples): add defaults for SAC in Walker2d
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/SAC/walker_defaults.py
@@ -0,0 +1,72 @@
+"""Tune experiment configuration to test SAC in Hopper-v3."""
+from ray import tune
+
+
+def get_config():
+    return {
+        # === Environment ===
+        "env": "TimeLimitedEnv",
+        "env_config": {
+            "env_id": "Walker2d-v3",
+            "max_episode_steps": 1000,
+            "time_aware": False,
+        },
+        # === Twin Delayed DDPG (TD3) tricks ===
+        # Clipped Double Q-Learning: use the minimun of two target Q functions
+        # as the next action-value in the target for fitted Q iteration
+        "clipped_double_q": True,
+        # === Replay buffer ===
+        # Size of the replay buffer. Note that if async_updates is set, then
+        # each worker will have a replay buffer of this size.
+        "buffer_size": int(1e5),
+        # === Optimization ===
+        # PyTorch optimizer to use for policy
+        "policy_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # PyTorch optimizer to use for critic
+        "critic_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # PyTorch optimizer to use for entropy coefficient
+        "alpha_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # Interpolation factor in polyak averaging for target networks.
+        "polyak": 0.995,
+        # === Network ===
+        # Size and activation of the fully connected networks computing the logits
+        # for the policy and action-value function. No layers means the component is
+        # linear in states and/or actions.
+        "module": {
+            "policy": {
+                "units": (256, 256),
+                "activation": "ReLU",
+                "initializer_options": {"name": "xavier_uniform"},
+                "input_dependent_scale": True,
+            },
+            "critic": {
+                "units": (256, 256),
+                "activation": "ReLU",
+                "initializer_options": {"name": "xavier_uniform"},
+                "delay_action": True,
+            },
+        },
+        # === Exploration ===
+        # Until this many timesteps have elapsed, the agent's policy will be
+        # ignored & it will instead take uniform random actions. Can be used in
+        # conjunction with learning_starts (which controls when the first
+        # optimization step happens) to decrease dependence of exploration &
+        # optimization on initial policy parameters. Note that this will be
+        # disabled when the action noise scale is set to 0 (e.g during evaluation).
+        "pure_exploration_steps": 10000,
+        # === Trainer ===
+        "train_batch_size": 256,
+        "timesteps_per_iteration": 1000,
+        # === Evaluation ===
+        # Evaluate with every `evaluation_interval` training iterations.
+        # The evaluation stats will be reported under the "evaluation" metric key.
+        "evaluation_interval": 5,
+        # Number of episodes to run per evaluation period.
+        "evaluation_num_episodes": 5,
+        # === Debugging ===
+        # Set the ray.rllib.* log level for the agent process and its workers.
+        # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
+        # periodically print out summaries of relevant internal dataflow (this is
+        # also printed out once at startup at the INFO level).
+        "log_level": "WARN",
+    }
