1ccaf1fa2ceda82b73c9ac5e6975408af98b473b
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-30 15:06:05 -0300

    fix(tests): use updated configs module test for NAF
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/algorithms/naf/test_module.py
+++ tests/algorithms/naf/test_module.py
@@ -1,14 +1,9 @@
 # pylint: disable=missing-docstring,redefined-outer-name
 import pytest
+import numpy as np
 import torch.nn as nn
-from gym import spaces
 
 from raylab.algorithms.naf.naf_policy import NAFTorchPolicy
-
-
-@pytest.fixture(params=[None, "trace", "script"])
-def torch_script(request):
-    return request.param
 
 
 @pytest.fixture(params=[None, "diag_gaussian", "full_gaussian", "parameter_noise"])
@@ -22,28 +17,16 @@ def clipped_double_q(request):
 
 
 @pytest.fixture
-def config(torch_script, exploration, clipped_double_q):
+def config(exploration, clipped_double_q):
     return {
         "module": {
-            "layers": [32, 32],
+            "units": (32, 32),
             "activation": "ELU",
-            "initializer": "orthogonal",
-            "initializer_options": {"gain": 1.0},
+            "initializer_options": {"name": "orthogonal", "gain": np.sqrt(2)},
         },
-        "torch_script": torch_script,
         "exploration": exploration,
         "clipped_double_q": clipped_double_q,
     }
-
-
-@pytest.fixture
-def obs_space():
-    return spaces.Box(low=-1, high=1, shape=(4,))
-
-
-@pytest.fixture
-def action_space():
-    return spaces.Box(low=-1, high=1, shape=(2,))
 
 
 def test_make_module(obs_space, action_space, config):
