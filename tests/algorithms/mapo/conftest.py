06b6410fbd33ea223f1222013d43a188ecd9ce7f
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-30 15:22:01 -0300

    test: add critic tests for MAPO
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ tests/algorithms/mapo/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+from raylab.algorithms.registry import ALGORITHMS as ALGS
+
+
+@pytest.fixture
+def mapo_trainer():
+    return ALGS["MAPO"]()
+
+
+@pytest.fixture
+def mapo_policy(mapo_trainer):
+    return mapo_trainer._policy
+
+
+@pytest.fixture
+def policy_and_batch_fn(policy_and_batch_fn, mapo_policy):
+    def make_policy_and_batch(config):
+        return policy_and_batch_fn(mapo_policy, config)
+
+    return make_policy_and_batch
