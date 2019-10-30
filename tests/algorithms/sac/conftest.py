6e6e195c77c5fbfeaabe7829e225aac715be0490
Ângelo Lovatto <angelolovatto@gmail.com>
2019-10-30 17:11:35 -0300

    test: remove unused environment fixtures
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/algorithms/sac/conftest.py
+++ tests/algorithms/sac/conftest.py
@@ -15,13 +15,6 @@ def sac_policy(sac_trainer):
 
 
 @pytest.fixture
-def cartpole_swingup_env(envs):
-    return lambda _: envs["CartPoleSwingUp"](
-        {"time_aware": True, "max_episode_steps": 200}
-    )
-
-
-@pytest.fixture
 def policy_and_batch_fn(policy_and_batch_fn, sac_policy):
     def make_policy_and_batch(config):
         return policy_and_batch_fn(sac_policy, config)
