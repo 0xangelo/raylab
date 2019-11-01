8adc2df8f7c726326ccd3d6f64e60c3fcfc492e5
Ângelo Lovatto <angelolovatto@gmail.com>
2019-11-01 16:45:39 -0300

    feat(examples): add defaults for MAPO on Navigation
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/MAPO/navigation_true_model.py
@@ -0,0 +1,33 @@
+# pylint: disable=missing-docstring
+from ray import tune
+
+from navigation_base import get_config as base_config
+
+
+def get_config():
+    return {
+        **base_config(),
+        # === Environment ===
+        "env_config": tune.grid_search(
+            [
+                {"deceleration_zones": None},
+                {"deceleration_zones": {"center": [[0.0, 0.0]], "decay": [2.0]}},
+            ]
+        ),
+        # === MAPO model training ===
+        # Type of model-training to use. Possible types include
+        # decision_aware: policy gradient-aware model learning
+        # mle: maximum likelihood estimation
+        "model_loss": "decision_aware",
+        # Type of the used p-norm of the distance between gradients.
+        # Can be float('inf') for infinity norm.
+        "norm_type": 2,
+        # Number of next states to sample from the model when calculating the
+        # model-aware deterministic policy gradient
+        "num_model_samples": 4,
+        # Gradient estimator for model-aware dpg. Possible types include:
+        # score_function, pathwise_derivative
+        "grad_estimator": tune.grid_search(["score_function", "pathwise_derivative"]),
+        # Whether to use the environment's true model to sample states
+        "true_model": True,
+    }
