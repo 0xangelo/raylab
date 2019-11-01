c50f26a977bd78f7de2e4b5991fbac180e3a0b13
Ângelo Lovatto <angelolovatto@gmail.com>
2019-11-01 16:40:49 -0300

    feat(logger): add custom CSVLogger
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/logger/__init__.py
+++ raylab/logger/__init__.py
@@ -1,6 +1,6 @@
 """Custom loggers to use with Tune."""
-from ray.tune.logger import CSVLogger
 from raylab.logger.config_json_logger import ConfigJsonLogger
+from raylab.logger.csv_logger import CSVLogger
 from raylab.logger.torch_tensorboard_logger import TorchTBLogger
 
 DEFAULT_LOGGERS = (ConfigJsonLogger, CSVLogger, TorchTBLogger)
