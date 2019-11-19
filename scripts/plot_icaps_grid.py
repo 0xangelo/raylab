791cad4663593d1868a6d2297ec028d8807763b4
Ângelo Lovatto <angelolovatto@gmail.com>
2019-11-19 15:15:58 -0300

    feat(scripts): add Reservoir experiments
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- scripts/plot_icaps_grid.py
+++ scripts/plot_icaps_grid.py
@@ -96,6 +96,60 @@ def plot_navigation_grid(local_dir):
         plt.show()
 
 
+def plot_reservoir_grid(local_dir):
+    """Plot experiment comparative grid."""
+
+    path_fmt = osp.join(local_dir, "{}-Reservoir-{}/")
+    args = dict(
+        x="timesteps_total",
+        y="evaluation/episode_reward_mean",
+        hue="algorithm",
+        units=None,
+        estimator="mean",
+        err_style="band",
+    )
+
+    cols = "Walks NonDiagCov LinearModel".split()
+
+    with sns.plotting_context("paper"):
+        fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(4, 8))
+
+        for j, col in enumerate(cols):
+            exps_data = core.load_exps_data(
+                path_fmt.format("MAPO", col)
+            ) + core.load_exps_data(path_fmt.format("SOP", col))
+            exps_data = list(
+                map(process_deceleration_zones, map(process_algorithm_name, exps_data))
+            )
+            core.insert_params_dataframe(exps_data, "algorithm")
+            selectors, titles = core.filter_and_split_experiments(exps_data)
+            plot_inst = core.lineplot_instructions(selectors, titles, **args)[0]
+            plot_kwargs = plot_inst["lineplot_kwargs"]
+            sns.lineplot(
+                ax=axes[1][j], legend="full" if j == 2 else False, **plot_kwargs
+            )
+
+        # Just some formatting niceness:
+        # x-axis scale in scientific notation if max x is large
+        plt.ticklabel_format(style="sci", axis="x", scilimits=(0, 0))
+
+        pad = 5  # in points
+        for ax, col in zip(axes[0], cols):
+            ax.annotate(
+                col,
+                xy=(0.5, 1),
+                xytext=(0, pad),
+                xycoords="axes fraction",
+                textcoords="offset points",
+                size="large",
+                ha="center",
+                va="baseline",
+            )
+
+        fig.tight_layout()
+        plt.show()
+
+
 @click.command()
 @click.option(
     "--local-dir",
@@ -117,6 +171,8 @@ def cli(local_dir, alg_name):
     """Plot grid of experiment comparisons."""
     if alg_name == "Navigation":
         plot_navigation_grid(local_dir)
+    elif alg_name == "Reservoir":
+        plot_reservoir_grid(local_dir)
     else:
         raise ValueError(f"Unrecognized algorithm name {alg_name}")
 
