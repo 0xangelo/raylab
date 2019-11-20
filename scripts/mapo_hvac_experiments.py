7eb5f2b8d9c7c34ee8b9c3664934c62d78baf68a
Ângelo Lovatto <angelolovatto@gmail.com>
2019-11-20 00:20:24 -0300

    feat(scripts): add HVAC experiments
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ scripts/mapo_hvac_experiments.py
@@ -0,0 +1,286 @@
+"""Script for launching Tune experiments for MAPO."""
+import logging
+
+import click
+import ray
+from ray import tune
+import numpy as np
+
+import raylab
+from raylab.logger import DEFAULT_LOGGERS as CUSTOM_LOGGERS
+from raylab.envs.hvac import DEFAULT_CONFIG as ENV_CONFIG
+from plot_icaps_grid import plot_hvac_grid
+
+
+SEEDS = 6
+
+STOP_COND = {"timesteps_total": int(1e3)}
+
+ACTOR_CRITIC_CONFIG = {
+    "policy": {
+        "units": (128, 128),
+        "activation": "ELU",
+        "initializer_options": {"name": "xavier_uniform", "gain": np.sqrt(2)},
+    },
+    "critic": {
+        "units": (128, 128),
+        "activation": "ELU",
+        "initializer_options": {"name": "xavier_uniform", "gain": np.sqrt(2)},
+        "delay_action": True,
+    },
+}
+
+Q_LEARNING_CONFIG = {
+    # === SQUASHING EXPLORATION PROBLEM ===
+    # Maximum l1 norm of the policy's output vector before the squashing function
+    "beta": 1.2,
+    # === Twin Delayed DDPG (TD3) tricks ===
+    # Clipped Double Q-Learning: use the minimun of two target Q functions
+    # as the next action-value in the target for fitted Q iteration
+    "clipped_double_q": True,
+    # Add gaussian noise to the action when calculating the Deterministic
+    # Policy Gradient
+    "target_policy_smoothing": True,
+    # Additive Gaussian i.i.d. noise to add to actions inputs to target Q function
+    "target_gaussian_sigma": 0.3,
+    # Interpolation factor in polyak averaging for target networks.
+    "polyak": 0.995,
+}
+
+REPLAY_CONFIG = {
+    # === Replay Buffer ===
+    "buffer_size": int(2e4)
+}
+
+EXPLORATION_CONFIG = {
+    # === Exploration ===
+    # Which type of exploration to use. Possible types include
+    # None: use the greedy policy to act
+    # parameter_noise: use parameter space noise
+    # gaussian: use i.i.d gaussian action space noise independently for each
+    #     action dimension
+    "exploration": "gaussian",
+    # Additive Gaussian i.i.d. noise to add to actions before squashing
+    "exploration_gaussian_sigma": 0.3,
+    # Until this many timesteps have elapsed, the agent's policy will be
+    # ignored & it will instead take uniform random actions. Can be used in
+    # conjunction with learning_starts (which controls when the first
+    # optimization step happens) to decrease dependence of exploration &
+    # optimization on initial policy parameters. Note that this will be
+    # disabled when the action noise scale is set to 0 (e.g during evaluation).
+    "pure_exploration_steps": 200,
+}
+
+EVALUATION_CONFIG = {
+    # === Evaluation ===
+    "evaluation_interval": 5,
+    "evaluation_num_episodes": 5,
+}
+
+TRAINER_CONFIG = {
+    # === RolloutWorker ===
+    "sample_batch_size": 1,
+    "batch_mode": "complete_episodes",
+    # === Trainer ===
+    "train_batch_size": 32,
+    "timesteps_per_iteration": 400,
+    # === Debugging ===
+    # Set the ray.rllib.* log level for the agent process and its workers.
+    # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
+    # periodically print out summaries of relevant internal dataflow (this is
+    # also printed out once at startup at the INFO level).
+    "log_level": "WARN",
+}
+
+
+MAPO_CONFIG = {
+    # === MAPO model training ===
+    # Type of model-training to use. Possible types include
+    # decision_aware: policy gradient-aware model learning
+    # mle: maximum likelihood estimation
+    "model_loss": tune.grid_search(["decision_aware", "mle"]),
+    # Gradient estimator for model-aware dpg. Possible types include:
+    # score_function, pathwise_derivative
+    "grad_estimator": tune.grid_search(["score_function", "pathwise_derivative"]),
+    # Type of the used p-norm of the distance between gradients.
+    # Can be float('inf') for infinity norm.
+    "norm_type": 2,
+    # Number of next states to sample from the model when calculating the
+    # model-aware deterministic policy gradient
+    "num_model_samples": 16,
+    # === Optimization ===
+    # PyTorch optimizer to use for policy
+    "policy_optimizer": {"name": "RMSprop", "options": {"lr": 1e-4}},
+    # PyTorch optimizer to use for critic
+    "critic_optimizer": {"name": "RMSprop", "options": {"lr": 1e-4}},
+    # PyTorch optimizer to use for model
+    "model_optimizer": {"name": "RMSprop", "options": {"lr": 1e-4}},
+}
+
+SOP_CONFIG = {
+    # === Optimization ===
+    # PyTorch optimizer to use for policy
+    "policy_optimizer": {"name": "RMSprop", "options": {"lr": 1e-4}},
+    # PyTorch optimizer to use for critic
+    "critic_optimizer": {"name": "RMSprop", "options": {"lr": 1e-4}},
+    # === Network ===
+    # Size and activation of the fully connected networks computing the logits
+    # for the policy and action-value function. No layers means the component is
+    # linear in states and/or actions.
+    "module": ACTOR_CRITIC_CONFIG,
+}
+
+
+@click.command()
+@click.option(
+    "--local-dir",
+    "-l",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True),
+    default="data/",
+    show_default=True,
+    help="",
+)
+@click.option(
+    "--checkpoint-freq",
+    type=int,
+    default=0,
+    show_default=True,
+    help="How many training iterations between checkpoints. "
+    "A value of 0 disables checkpointing.",
+)
+@click.option(
+    "--checkpoint-at-end",
+    type=bool,
+    default=True,
+    show_default=True,
+    help="Whether to checkpoint at the end of the experiment regardless of "
+    "the checkpoint_freq.",
+)
+@click.option(
+    "--object-store-memory",
+    type=int,
+    default=int(2e9),
+    show_default=True,
+    help="The amount of memory (in bytes) to start the object store with. "
+    "By default, this is capped at 20GB but can be set higher.",
+)
+@click.option(
+    "--tune-log-level",
+    type=str,
+    default="WARN",
+    show_default=True,
+    help="Logging level for the trial executor process. This is independent from each "
+    "trainer's logging level.",
+)
+def experiment(**args):
+    """Launch a Tune experiment for MAPO on HVAC."""
+    raylab.register_all_agents()
+    raylab.register_all_environments()
+    ray.init(object_store_memory=args["object_store_memory"])
+    logging.getLogger("ray.tune").setLevel(args["tune_log_level"])
+
+    def run_mapo_vs_sop(mapo_config, sop_config, exp_suffix):
+        tune.run(
+            "MAPO",
+            name=f"MAPO-HVAC-{exp_suffix}",
+            local_dir=args["local_dir"],
+            stop=STOP_COND,
+            config=mapo_config,
+            checkpoint_freq=args["checkpoint_freq"],
+            checkpoint_at_end=args["checkpoint_at_end"],
+            loggers=CUSTOM_LOGGERS,
+        )
+        tune.run(
+            "SOP",
+            name=f"SOP-HVAC-{exp_suffix}",
+            local_dir=args["local_dir"],
+            stop=STOP_COND,
+            config=sop_config,
+            checkpoint_freq=args["checkpoint_freq"],
+            checkpoint_at_end=args["checkpoint_at_end"],
+            loggers=CUSTOM_LOGGERS,
+        )
+
+    base_config = {
+        **Q_LEARNING_CONFIG,
+        **REPLAY_CONFIG,
+        **EXPLORATION_CONFIG,
+        **EVALUATION_CONFIG,
+        **TRAINER_CONFIG,
+        "seed": tune.grid_search(list(range(SEEDS))),
+    }
+
+    # === Random Walk Experiments ===
+    env_config = {
+        "env": "HVAC",
+        "env_config": {
+            **ENV_CONFIG,
+            "random_walks": {"num_walks": 16, "loc": 10.0, "scale": 2.0},
+        },
+    }
+    mapo_config = {
+        **base_config,
+        **env_config,
+        **MAPO_CONFIG,
+        "module": {
+            **ACTOR_CRITIC_CONFIG,
+            "model": {
+                "units": (len(env_config["env_config"]["CAP"]) + 1,),
+                "activation": None,
+                "initializer_options": {"name": "xavier_uniform"},
+                "delay_action": False,
+                "input_dependent_scale": False,
+            },
+        },
+    }
+    sop_config = {**base_config, **env_config, **SOP_CONFIG}
+    run_mapo_vs_sop(mapo_config, sop_config, "Walks")
+
+    # === Original Experiments ===
+    env_config = {"env": "HVAC"}
+    mapo_config = {
+        **base_config,
+        **env_config,
+        **MAPO_CONFIG,
+        "module": {
+            **ACTOR_CRITIC_CONFIG,
+            "model": {
+                "units": (128, 128),
+                "activation": "ELU",
+                "initializer_options": {"name": "xavier_uniform", "gain": np.sqrt(2)},
+                "delay_action": True,
+                "input_dependent_scale": False,
+            },
+        },
+    }
+    sop_config = {**base_config, **env_config, **SOP_CONFIG}
+    run_mapo_vs_sop(mapo_config, sop_config, "Original")
+
+    # === Linear Model Experiments ===
+    env_config = {
+        # === Environment ===
+        "env": "HVAC"
+    }
+    mapo_config = {
+        **base_config,
+        **env_config,
+        **MAPO_CONFIG,
+        "module": {
+            **ACTOR_CRITIC_CONFIG,
+            "model": {
+                "units": (),
+                "activation": None,
+                "initializer_options": {"name": "xavier_uniform"},
+                "delay_action": False,
+                "input_dependent_scale": False,
+            },
+        },
+    }
+    sop_config = {**base_config, **env_config, **SOP_CONFIG}
+    run_mapo_vs_sop(mapo_config, sop_config, "LinearModel")
+
+    plot_hvac_grid(args["local_dir"])
+
+
+if __name__ == "__main__":
+    experiment()
--- scripts/plot_icaps_grid.py
+++ scripts/plot_icaps_grid.py
@@ -149,6 +149,68 @@ def plot_reservoir_grid(local_dir):
         plt.show()
 
 
+def plot_hvac_grid(local_dir):
+    """Plot experiment comparative grid."""
+
+    path_fmt = osp.join(local_dir, "{}-HVAC-{}/")
+    args = dict(
+        # x="timesteps_total",
+        x="iterations_since_restore",
+        y="evaluation/episode_reward_mean",
+        hue="algorithm",
+        style="grad_estimator",
+        units=None,
+        estimator="mean",
+        err_style="band",
+    )
+
+    def process_params(exp_data):
+        exp_data = process_algorithm_name(exp_data)
+        exp_data = process_grad_estimator(exp_data)
+        return exp_data
+
+    cols = "Walks Original LinearModel".split()
+
+    with sns.plotting_context("paper"):
+        fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(15, 10))
+
+        for j, col in enumerate(cols):
+            exps_data = core.load_exps_data(
+                path_fmt.format("MAPO", col)
+            ) + core.load_exps_data(path_fmt.format("SOP", col))
+            exps_data = list(map(process_params, exps_data))
+            core.insert_params_dataframe(exps_data, "algorithm", "grad_estimator")
+            selectors, titles = core.filter_and_split_experiments(exps_data)
+            inst = core.lineplot_instructions(selectors, titles, **args)[0]
+            plot_kwargs = inst["lineplot_kwargs"]
+            sns.lineplot(
+                ax=axes[j],
+                legend="full" if j == 2 else False,
+                palette=["#1f77b4", "#ff7f0e", "grey"],
+                **plot_kwargs,
+            )
+
+        # Just some formatting niceness:
+        # x-axis scale in scientific notation if max x is large
+        plt.ticklabel_format(style="sci", axis="x", scilimits=(0, 0))
+
+        pad = 5  # in points
+        for ax, col in zip(axes, cols):
+            ax.annotate(
+                col,
+                xy=(0.5, 1),
+                xytext=(0, pad),
+                xycoords="axes fraction",
+                textcoords="offset points",
+                size="large",
+                ha="center",
+                va="baseline",
+            )
+
+        fig.tight_layout()
+        plt.show()
+
+
 @click.command()
 @click.option(
     "--local-dir",
@@ -172,6 +234,8 @@ def cli(local_dir, alg_name):
         plot_navigation_grid(local_dir)
     elif alg_name == "Reservoir":
         plot_reservoir_grid(local_dir)
+    elif alg_name == "HVAC":
+        plot_hvac_grid(local_dir)
     else:
         raise ValueError(f"Unrecognized algorithm name {alg_name}")
 
