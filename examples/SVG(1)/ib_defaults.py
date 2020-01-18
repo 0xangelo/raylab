5d4d428c514a0e5cc4237684256815814a99ffc6
Ângelo Lovatto <angelolovatto@gmail.com>
2020-01-18 14:35:12 -0300

    feat(examples): add defaults for SVG(1) on IB
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/SVG(1)/ib_defaults.py
@@ -0,0 +1,73 @@
+# pylint: disable=missing-docstring
+
+
+def get_config():
+    return {
+        # === Environment ===
+        "env": "IndustrialBenchmark",
+        "env_config": {
+            "reward_type": "classic",
+            "action_type": "continuous",
+            "observation": "markovian",
+            "max_episode_steps": 1000,
+            "time_aware": True,
+        },
+        # === Replay buffer ===
+        # Size of the replay buffer. Note that if async_updates is set, then
+        # each worker will have a replay buffer of this size.
+        "buffer_size": int(1e5),
+        # === Optimization ===
+        # Weight of the fitted V loss in the joint model-value loss
+        "vf_loss_coeff": 1.0,
+        # Clip gradient norms by this value
+        "max_grad_norm": float("inf"),
+        # === Regularization ===
+        "kl_schedule": {
+            "initial_coeff": 0.0,
+            "desired_kl": 0.01,
+            "adaptation_coeff": 2.0,
+            "threshold": 1.5,
+        },
+        # === Optimization ===
+        # PyTorch optimizer to use
+        "torch_optimizer": "Adam",
+        # Optimizer options for each component.
+        # Valid keys include: 'model', 'value', and 'policy'
+        "torch_optimizer_options": {
+            "model": {"lr": 1e-3},
+            "value": {"lr": 1e-3},
+            "policy": {"lr": 1e-3},
+        },
+        # === Regularization ===
+        # Whether to penalize KL divergence with the current policy or past policies
+        # that generated the replay pool.
+        "replay_kl": True,
+        # === Network ===
+        # Size and activation of the fully connected networks computing the logits
+        # for the policy, value function and model. No layers means the component is
+        # linear in states and/or actions.
+        "module": {
+            "policy": {
+                "layers": [100, 100],
+                "activation": "Tanh",
+                "initializer_options": {"name": "xavier_uniform"},
+                "input_dependent_scale": False,
+            },
+            "value": {
+                "layers": [400, 200],
+                "activation": "Tanh",
+                "initializer_options": {"name": "xavier_uniform"},
+            },
+            "model": {
+                "layers": [40, 40],
+                "activation": "Tanh",
+                "initializer_options": {"name": "xavier_uniform"},
+                "delay_action": True,
+            },
+        },
+        # === Trainer ===
+        "train_batch_size": 128,
+        "timesteps_per_iteration": 1000,
+        # === Evaluation ===
+        "evaluation_interval": 5,
+    }
