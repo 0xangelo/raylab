c6616deb4c9a2444d03d776ddbef76edd46597fb
Ângelo Lovatto <angelolovatto@gmail.com>
2020-02-15 16:47:32 -0300

    feat(utils): move get_agent to experiments utilities
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/cli/evaluate_checkpoint.py
+++ raylab/cli/evaluate_checkpoint.py
@@ -4,53 +4,20 @@ import click
 from .utils import initialize_raylab
 
 
-def get_agent(checkpoint, algo, env):
-    """Instatiate and restore agent class from checkpoint."""
-    import os
-    import pickle
-    from ray.tune.registry import TRAINABLE_CLASS, _global_registry
-    from ray.rllib.utils import merge_dicts
-
-    config = {}
-    # Load configuration from file
-    config_dir = os.path.dirname(checkpoint)
-    config_path = os.path.join(config_dir, "params.pkl")
-    if not os.path.exists(config_path):
-        config_path = os.path.join(config_dir, "../params.pkl")
-    if not os.path.exists(config_path):
-        raise ValueError(
-            "Could not find params.pkl in either the checkpoint dir or "
-            "its parent directory."
-        )
-    with open(config_path, "rb") as file:
-        config = pickle.load(file)
-
-    if "evaluation_config" in config:
-        eval_conf = config["evaluation_config"]
-        config = merge_dicts(config, eval_conf)
-
-    agent_cls = _global_registry.get(TRAINABLE_CLASS, algo)
-    agent = agent_cls(env=env, config=config)
-    agent.restore(checkpoint)
-    return agent
-
-
 @click.command()
 @click.argument("checkpoint")
-@click.option("--algo", default=None, help="Name of the trainable class to run.")
+@click.option(
+    "--algo", required=True, default=None, help="Name of the trainable class to run."
+)
 @click.option(
     "--env", default=None, help="Name of the environment to interact with, optional."
 )
-@click.pass_context
 @initialize_raylab
-def rollout(ctx, checkpoint, algo, env):
+def rollout(checkpoint, algo, env):
     """Simulate an agent from a given checkpoint in the desired environment."""
     from contextlib import suppress
-
     import ray
-
-    if not algo:
-        ctx.exit()
+    from raylab.utils.experiments import get_agent
 
     ray.init()
     agent = get_agent(checkpoint, algo, env)
