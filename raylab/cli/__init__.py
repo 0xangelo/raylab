ef70e4865e41223f5be372d6960b70ac4691dcf2
Ângelo Lovatto <angelolovatto@gmail.com>
2020-02-18 17:35:33 -0600

    feat(cli): add experiment dashboard
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/cli/__init__.py
+++ raylab/cli/__init__.py
@@ -12,6 +12,20 @@ def cli():
     """RayLab: Reinforcement learning algorithms in RLlib."""
 
 
+@cli.command()
+@click.argument(
+    "paths",
+    nargs=-1,
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=False),
+)
+def dashboard(paths):
+    """Launch the experiment dashboard to monitor training progress."""
+    from streamlit.cli import _main_run
+    from . import experiment_dashboard
+
+    _main_run(experiment_dashboard.__file__, paths)
+
+
 cli.add_command(experiment)
 cli.add_command(find_best)
 cli.add_command(rollout)
