f6026d6640470c919a032cb3a75c01c641d18413
Ângelo Lovatto <angelolovatto@gmail.com>
2020-03-30 07:42:03 -0300

    test(modules): test StochasticActorMixin with discrete actions
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/utils/debug.py
+++ raylab/utils/debug.py
@@ -1,19 +1,26 @@
 """Debugging utilities."""
-from numpy.random import uniform, randn
+import numpy as np
 from ray.rllib.policy.sample_batch import SampleBatch
+import gym.spaces as spaces
+
+
+def fake_space_samples(space, batch_size):
+    """Create fake samples from a Gym space."""
+    if isinstance(space, spaces.Box):
+        shape = (batch_size,) + space.shape
+        return np.clip(np.random.randn(*shape), space.low, space.high)
+    if isinstance(space, spaces.Discrete):
+        return np.random.randint(space.n, size=batch_size)
+    raise ValueError(f"Unsupported space type {type(space)}")
 
 
 def fake_batch(obs_space, action_space, batch_size=1):
     """Create a fake SampleBatch compatible with Policy.learn_on_batch."""
-    obs_sample_shape = (batch_size,) + obs_space.shape
-    act_sample_shape = (batch_size,) + action_space.shape
     samples = {
-        SampleBatch.CUR_OBS: randn(*obs_sample_shape),
-        SampleBatch.ACTIONS: uniform(
-            action_space.low, action_space.high, size=act_sample_shape
-        ),
-        SampleBatch.REWARDS: randn(batch_size),
-        SampleBatch.NEXT_OBS: randn(*obs_sample_shape),
-        SampleBatch.DONES: randn(batch_size) > 0,
+        SampleBatch.CUR_OBS: fake_space_samples(obs_space, batch_size),
+        SampleBatch.ACTIONS: fake_space_samples(action_space, batch_size),
+        SampleBatch.REWARDS: np.random.randn(batch_size),
+        SampleBatch.NEXT_OBS: fake_space_samples(obs_space, batch_size),
+        SampleBatch.DONES: np.random.randn(batch_size) > 0,
     }
     return SampleBatch(samples)
--- tests/modules/test_stochastic_actor_mixin.py
+++ tests/modules/test_stochastic_actor_mixin.py
@@ -3,12 +3,27 @@ from functools import partial
 
 import pytest
 import torch
+from gym.spaces import Box, Discrete
 from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.utils.tracking_dict import UsageTrackingDict
 
-from raylab.modules.catalog import SACModule, SVGModule
+from raylab.utils.debug import fake_batch
+from raylab.utils.pytorch import convert_to_tensor
+
+from raylab.modules.catalog import SACModule, SVGModule, TRPOModule
+from raylab.modules.action_value_mixin import ActionValueMixin
+from raylab.modules.stochastic_model_mixin import StochasticModelMixin
 
 
-@pytest.fixture(params=(SACModule, SVGModule))
+@pytest.fixture(
+    params=(Box(-1, 1, shape=(1,)), Box(-1, 1, shape=(3,)), Discrete(2), Discrete(8),),
+    ids=tuple("Box((1,)) Box((3,)) Discrete(2) Discrete(8)".split()),
+)
+def action_space(request):
+    return request.param
+
+
+@pytest.fixture(params=(SACModule, SVGModule, TRPOModule))
 def module_cls(request):
     return request.param
 
@@ -24,12 +39,29 @@ def mean_action_only(request):
 
 
 @pytest.fixture
-def module_batch_fn(module_and_batch_fn, module_cls):
-    return partial(module_and_batch_fn, module_cls)
+def module_batch_spaces_fn(module_cls, obs_space, action_space, torch_script):
+    is_discrete = isinstance(action_space, Discrete)
+    if issubclass(module_cls, (ActionValueMixin, StochasticModelMixin)) and is_discrete:
+        pytest.skip(
+            "ActionValueMixin and StochasticModelMixin are currently incompatible with "
+            "discrete action spaces"
+        )
+
+    def make_module_batch_spaces(config):
+        config["torch_script"] = torch_script
+        module = module_cls(obs_space, action_space, config)
+        module = torch.jit.script(module) if torch_script else module
+
+        batch = UsageTrackingDict(fake_batch(obs_space, action_space, batch_size=10))
+        batch.set_get_interceptor(partial(convert_to_tensor, device="cpu"))
+
+        return module, batch, obs_space, action_space
+
+    return make_module_batch_spaces
 
 
-def test_actor_sampler(module_batch_fn, input_dependent_scale, mean_action_only):
-    module, batch = module_batch_fn(
+def test_actor_sampler(module_batch_spaces_fn, input_dependent_scale, mean_action_only):
+    module, batch, _, action_space = module_batch_spaces_fn(
         {
             "mean_action_only": mean_action_only,
             "actor": {"input_dependent_scale": input_dependent_scale},
@@ -37,57 +69,75 @@ def test_actor_sampler(module_batch_fn, input_dependent_scale, mean_action_only)
     )
     action = batch[SampleBatch.ACTIONS]
 
-    samples, logp = module.actor.rsample(batch[SampleBatch.CUR_OBS])
-    samples_, _ = module.actor.rsample(batch[SampleBatch.CUR_OBS])
+    sampler = (
+        module.actor.rsample if isinstance(action_space, Box) else module.actor.sample
+    )
+    samples, logp = sampler(batch[SampleBatch.CUR_OBS])
+    samples_, _ = sampler(batch[SampleBatch.CUR_OBS])
     assert samples.shape == action.shape
-    assert samples.dtype == torch.float32
+    assert samples.dtype == action.dtype
     assert logp.shape == batch[SampleBatch.REWARDS].shape
-    assert samples.dtype == torch.float32
+    assert logp.dtype == batch[SampleBatch.REWARDS].dtype
     assert mean_action_only or not torch.allclose(samples, samples_)
 
 
-def test_actor_params(module_batch_fn, input_dependent_scale):
-    module, batch = module_batch_fn(
+def test_actor_params(module_batch_spaces_fn, input_dependent_scale):
+    module, batch, _, action_space = module_batch_spaces_fn(
         {"actor": {"input_dependent_scale": input_dependent_scale}}
     )
 
     params = module.actor(batch[SampleBatch.CUR_OBS])
-    assert "loc" in params
-    assert "scale" in params
+    if isinstance(action_space, Box):
+        assert "loc" in params
+        assert "scale" in params
 
-    loc, scale = params.values()
-    action = batch[SampleBatch.ACTIONS]
-    assert loc.shape == action.shape
-    assert scale.shape == action.shape
-    assert loc.dtype == torch.float32
-    assert scale.dtype == torch.float32
+        loc, scale = params["loc"], params["scale"]
+        action = batch[SampleBatch.ACTIONS]
+        assert loc.shape == action.shape
+        assert scale.shape == action.shape
+        assert loc.dtype == torch.float32
+        assert scale.dtype == torch.float32
 
-    pi_params = set(module.actor.parameters())
-    for par in pi_params:
-        par.grad = None
-    loc.mean().backward()
-    assert any(p.grad is not None for p in pi_params)
-    assert all(p.grad is None for p in set(module.parameters()) - pi_params)
+        pi_params = set(module.actor.parameters())
+        for par in pi_params:
+            par.grad = None
+        loc.mean().backward()
+        assert any(p.grad is not None for p in pi_params)
+        assert all(p.grad is None for p in set(module.parameters()) - pi_params)
 
-    for par in pi_params:
-        par.grad = None
-    module.actor(batch[SampleBatch.CUR_OBS])["scale"].mean().backward()
-    assert any(p.grad is not None for p in pi_params)
-    assert all(p.grad is None for p in set(module.parameters()) - pi_params)
+        for par in pi_params:
+            par.grad = None
+        module.actor(batch[SampleBatch.CUR_OBS])["scale"].mean().backward()
+        assert any(p.grad is not None for p in pi_params)
+        assert all(p.grad is None for p in set(module.parameters()) - pi_params)
+    else:
+        assert "logits" in params
+        logits = params["logits"]
+        assert logits.shape[-1] == action_space.n
+
+        pi_params = set(module.actor.parameters())
+        for par in pi_params:
+            par.grad = None
+        logits.mean().backward()
+        assert any(p.grad is not None for p in pi_params)
+        assert all(p.grad is None for p in set(module.parameters()) - pi_params)
 
 
-def test_actor_reproduce(module_batch_fn, input_dependent_scale):
-    module, batch = module_batch_fn(
+def test_actor_reproduce(module_batch_spaces_fn, input_dependent_scale):
+    module, batch, _, action_space = module_batch_spaces_fn(
         {"actor": {"input_dependent_scale": input_dependent_scale}}
     )
 
     acts = batch[SampleBatch.ACTIONS]
     _acts = module.actor.reproduce(batch[SampleBatch.CUR_OBS], acts)
-    assert _acts.shape == acts.shape
-    assert _acts.dtype == acts.dtype
-    assert torch.allclose(_acts, acts, atol=1e-6)
+    if isinstance(action_space, Discrete):
+        assert _acts is None
+    else:
+        assert _acts.shape == acts.shape
+        assert _acts.dtype == acts.dtype
+        assert torch.allclose(_acts, acts, atol=1e-6)
 
-    _acts.mean().backward()
-    pi_params = set(module.actor.parameters())
-    assert all(p.grad is not None for p in pi_params)
-    assert all(p.grad is None for p in set(module.parameters()) - pi_params)
+        _acts.mean().backward()
+        pi_params = set(module.actor.parameters())
+        assert all(p.grad is not None for p in pi_params)
+        assert all(p.grad is None for p in set(module.parameters()) - pi_params)
