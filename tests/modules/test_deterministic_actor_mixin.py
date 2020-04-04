1808b1089d362b37558cc7bbccbcb7f7748afb49
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-04 20:33:32 -0300

    chore(tests): reduce the number of tests for deterministic actor
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/modules/test_deterministic_actor_mixin.py
+++ tests/modules/test_deterministic_actor_mixin.py
@@ -1,12 +1,46 @@
 # pylint: disable=missing-docstring,redefined-outer-name,protected-access
 import pytest
 import torch
+import torch.nn as nn
 from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.utils import merge_dicts
 
-from raylab.modules.catalog import DDPGModule, MAPOModule
+from raylab.modules.deterministic_actor_mixin import DeterministicActorMixin
 
 
-@pytest.fixture(scope="module", params=(DDPGModule, MAPOModule))
+BASE_CONFIG = {
+    "torch_script": False,
+    "double_q": False,
+    "exploration": None,
+    "exploration_gaussian_sigma": 0.3,
+    "smooth_target_policy": False,
+    "target_gaussian_sigma": 0.3,
+    "actor": {
+        "units": (32, 32),
+        "activation": "ReLU",
+        "initializer_options": {"name": "xavier_uniform"},
+        "beta": 1.2,
+    },
+    "critic": {
+        "units": (32, 32),
+        "activation": "ReLU",
+        "initializer_options": {"name": "xavier_uniform"},
+        "delay_action": True,
+    },
+}
+
+
+class DummyModule(DeterministicActorMixin, nn.ModuleDict):
+    # pylint:disable=abstract-method
+
+    def __init__(self, obs_space, action_space, config):
+        super().__init__()
+        self.update(
+            self._make_actor(obs_space, action_space, merge_dicts(BASE_CONFIG, config))
+        )
+
+
+@pytest.fixture(scope="module", params=(DummyModule,))
 def module_cls(request):
     return request.param
 
