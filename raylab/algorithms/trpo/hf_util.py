fix(trpo): stabilize Natural gradient with fixed action samples
Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

raylab/algorithms/trpo/hf_util.py
@@ -5,26 +5,16 @@ import torch
 from torch.autograd import grad
 
 
-def fisher_vec_prod(vec, obs, policy, n_samples=1, damping=1e-3):
-    """Approximately compute the fisher-vector-product using samples.
-
-    This is based on the Fisher Matrix formulation as the expected hessian
-    of the negative log likelihood. For more information, see:
-    https://en.wikipedia.org/wiki/Fisher_information#Matrix_form
+def hessian_vector_product(output, params, vector):
+    """Computes the Hessian vector product w.r.t to a scalar loss.
 
     Args:
-        vec (Tensor): The vector to compute the Fisher vector product with.
-        obs (Tensor): The observations to evaluate the policy in.
-        policy (nn.Module): The policy
-        n_samples (int): The number of actions to sample for each state.
-        damping (float): Regularization to prevent the Fisher from becoming singular.
+        output (Tensor): loss tensor w.r.t. which the Hessian will be computed.
+        params (list): the parameters of the module, usually from a call to
+            `module.parameters()`.
+        vector (Tensor): The flattened vector to compute the Hessian product with.
+            This must have the same total number of elements in `params`.
     """
-    _, cur_logp = policy.sample(obs, sample_shape=(n_samples,))
-    fvp = -hessian_vector_product(cur_logp.mean(), policy.parameters(), vec)
-    return fvp + vec * damping
-
-
-def hessian_vector_product(output, params, vector):
     # pylint:disable=missing-docstring
     params = list(params)
     vecs, idx = [], 0

raylab/algorithms/trpo/trpo_policy.py
@@ -105,7 +105,6 @@ class TRPOTorchPolicy(TorchPolicy):
         surr_loss = -(self.module.actor.log_prob(cur_obs, actions) * advantages).mean()
         pol_grad = torch_util.flat_grad(surr_loss, self.module.actor.parameters())
         info["grad_norm(pg)"] = pol_grad.norm().item()
-        pol_grad = pol_grad / pol_grad.norm()
 
         # Compute Natural Gradient
         descent_step, cg_info = self._compute_descent_step(pol_grad, cur_obs)
@@ -133,20 +132,40 @@ class TRPOTorchPolicy(TorchPolicy):
             info["perplexity"] = torch.mean(-old_logp).exp().item()
         return {LEARNER_STATS_KEY: info}
 
-    def _compute_descent_step(self, pol_grad, cur_obs):
-        def fvp(vec):
-            return hf_util.fisher_vec_prod(
-                vec, cur_obs, self.module.actor, n_samples=self.config["fvp_samples"],
-            )
+    def _compute_descent_step(self, pol_grad, obs):
+        """Approximately compute the Natural gradient using samples.
 
-        descent_direction, cg_iters, cg_residual = hf_util.conjugate_gradient(
-            fvp, pol_grad
+        This is based on the Fisher Matrix formulation as the hessian of the average
+        entropy. For more information, see:
+        https://en.wikipedia.org/wiki/Fisher_information#Matrix_form
+
+        Args:
+            pol_grad (Tensor): The vector to compute the Fisher vector product with.
+            obs (Tensor): The observations to evaluate the policy in.
+        """
+        config = self.config
+        params = list(self.module.actor.parameters())
+        with torch.no_grad():
+            ent_acts, _ = self.module.actor.sample(obs, (config["fvp_samples"],))
+
+        def entropy():
+            return self.module.actor.log_prob(obs, ent_acts).neg().mean()
+
+        def fvp(vec):
+            return hf_util.hessian_vector_product(entropy(), params, vec)
+
+        descent_direction, elapsed_iters, residual = hf_util.conjugate_gradient(
+            lambda x: fvp(x) + config["cg_damping"] * x,
+            pol_grad,
+            cg_iters=config["cg_iters"],
         )
-        scale = torch.sqrt(
-            2 * self.config["delta"] / (pol_grad.dot(descent_direction) + 1e-8)
-        )
-        info = {"cg_iters": cg_iters, "cg_residual": cg_residual}
-        return descent_direction * scale, info
+
+        fisher_norm = pol_grad.dot(descent_direction)
+        delta = config["delta"]
+        scale = 0 if fisher_norm < 0 else torch.sqrt(2 * delta / (fisher_norm + 1e-8))
+
+        descent_direction = descent_direction * scale
+        return descent_direction, {"cg_iters": elapsed_iters, "cg_residual": residual}
 
     def _perform_line_search(self, pol_grad, descent_step, surr_loss, batch_tensors):
         expected_improvement = pol_grad.dot(descent_step).item()
@@ -165,7 +184,7 @@ class TRPOTorchPolicy(TorchPolicy):
             new_logp = self.module.actor.log_prob(cur_obs, actions)
             surr_loss = self._compute_surr_loss(old_logp, new_logp, advantages)
             avg_kl = torch.mean(old_logp - new_logp)
-            return surr_loss.item() if avg_kl < self.config["delta"] else float("inf")
+            return surr_loss.item() if avg_kl < self.config["delta"] else np.inf
 
         new_params, expected_improvement, improvement = hf_util.line_search(
             f_barrier,
@@ -175,10 +194,13 @@ class TRPOTorchPolicy(TorchPolicy):
             y_0=surr_loss.item(),
             **self.config["line_search_options"],
         )
+        improvement_ratio = (
+            improvement / expected_improvement if expected_improvement else np.nan
+        )
         info = {
             "expected_improvement": expected_improvement,
             "actual_improvement": improvement,
-            "improvement_ratio": improvement / expected_improvement,
+            "improvement_ratio": improvement_ratio,
         }
         return new_params, info
 