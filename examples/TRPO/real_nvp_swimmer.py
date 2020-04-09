59ab384973ff5d5e7937440fd4849feade630a29
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-09 08:30:34 -0300

    feat(examples): add defaults for RealNVP TRPO on Swimmer
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/TRPO/real_nvp_swimmer.py
@@ -0,0 +1,60 @@
+import numpy as np
+from ray import tune
+
+
+def get_config():
+    return {
+        # === Environment ===
+        "env": "Swimmer-v3",
+        "env_config": {"max_episode_steps": 400, "time_aware": False},
+        # Trust region constraint
+        "delta": 0.01,
+        # Number of actions to sample per state for Fisher vector product approximation
+        "fvp_samples": 10,
+        # For GAE(\gamma, \lambda)
+        "gamma": 0.99,
+        "lambda": 0.97,
+        # Number of iterations to fit value function
+        "val_iters": 40,
+        # Learning rate for critic optimizer
+        "val_lr": 1e-2,
+        # Whether to use Generalized Advantage Estimation
+        "use_gae": True,
+        # Whether to use a line search to calculate policy update.
+        # Effectively turns TRPO into Natural PG when turned off.
+        "line_search": True,
+        # === RolloutWorker ===
+        "num_workers": 0,
+        "num_envs_per_worker": 12,
+        "rollout_fragment_length": 400,
+        "batch_mode": "truncate_episodes",
+        "timesteps_per_iteration": 4800,
+        # === Network ===
+        # Size and activation of the fully connected networks computing the logits
+        # for the policy and value function. No layers means the component is
+        # linear in states or actions.
+        "module": {
+            "name": "TRPORealNVP",
+            "torch_script": False,
+            "actor": {
+                "obs_encoder": {
+                    "units": (32,),
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
+                "num_flows": 4,
+                "flow_mlp": {
+                    "units": (24,) * 2,
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
+            },
+            "critic": {
+                "encoder": {
+                    "units": (32, 32),
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
+            },
+        },
+    }
