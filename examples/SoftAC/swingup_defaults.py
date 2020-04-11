7490a0659b946776c11a30b3d635ca97fb8d1903
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-11 05:35:45 -0300

    chore(examples): update SAC defaults on Reacher & CartPoleSwingUp
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/SoftAC/swingup_defaults.py
+++ examples/SoftAC/swingup_defaults.py
@@ -1,12 +1,13 @@
-"""Tune experiment configuration to test SAC in MujocoReacher."""
 from ray import tune
 
 
 def get_config():
     return {
         # === Environment ===
-        "env": "MujocoReacher",
-        "env_config": {"max_episode_steps": 50, "time_aware": True},
+        "env": "CartPoleSwingUp",
+        "env_config": {"max_episode_steps": 500, "time_aware": False},
+        # === Replay Buffer ===
+        "buffer_size": int(1e5),
         # === Optimization ===
         # PyTorch optimizers to use
         "torch_optimizer": {
@@ -14,8 +15,6 @@ def get_config():
             "critics": {"type": "Adam", "lr": 3e-4},
             "alpha": {"type": "Adam", "lr": 3e-4},
         },
-        # === Replay Buffer ===
-        "buffer_size": 50000,
         # === Network ===
         # Size and activation of the fully connected networks computing the logits
         # for the policy and action-value function. No layers means the component is
@@ -35,24 +34,13 @@ def get_config():
         # Set to False for no exploration behavior (e.g., for evaluation).
         "explore": True,
         # Provide a dict specifying the Exploration object's config.
-        "exploration_config": {
-            # The Exploration class to use. In the simplest case, this is the name
-            # (str) of any class present in the `rllib.utils.exploration` package.
-            # You can also provide the python class directly or the full location
-            # of your class (e.g. "ray.rllib.utils.exploration.epsilon_greedy.
-            # EpsilonGreedy").
-            "type": "raylab.utils.exploration.StochasticActor",
-            # Options for parameter noise exploration
-            # Until this many timesteps have elapsed, the agent's policy will be
-            # ignored & it will instead take uniform random actions. Can be used in
-            # conjunction with learning_starts (which controls when the first
-            # optimization step happens) to decrease dependence of exploration &
-            # optimization on initial policy parameters. Note that this will be
-            # disabled when the action noise scale is set to 0 (e.g during evaluation).
-            "pure_exploration_steps": 500,
-        },
+        "exploration_config": {"pure_exploration_steps": 5000},
         # === Evaluation ===
         # Evaluate with every `evaluation_interval` training iterations.
         # The evaluation stats will be reported under the "evaluation" metric key.
         "evaluation_interval": 5,
+        "evaluation_config": {
+            "explore": False,
+            "env_config": {"max_episode_steps": 500, "time_aware": False},
+        },
     }
