db2a0c2cb1deaade62657aa2e61c3535158c5a6c
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-11 13:33:38 -0300

    Revert "chore(algorithms): rename SoftAC to SAC"
    This reverts commit d47af0044f856d989ab7435e0f19d356d3f9aae2.

--- raylab/algorithms/registry.py
+++ raylab/algorithms/registry.py
@@ -53,7 +53,7 @@ ALGORITHMS = {
     "NAF": _import_naf,
     "SVG(inf)": _import_svg_inf,
     "SVG(1)": _import_svg_one,
-    "SAC": _import_sac,
+    "SoftAC": _import_sac,
     "SOP": _import_sop,
     "MAPO": _import_mapo,
     "TRPO": _import_trpo,
