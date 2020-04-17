f853e98f7fcff684fec9b72ef0452532dfe2d81b
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-17 19:38:47 -0300

    test(networks): add checks for MLP and ResidualNet
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ tests/modules/networks/test_mlp.py
@@ -0,0 +1,22 @@
+# pylint:disable=missing-docstring,redefined-outer-name,protected-access
+import pytest
+import torch
+
+from raylab.modules.networks import MLP
+
+
+PARAMS = (None, {}, {"state": torch.randn(10, 4)})
+
+
+@pytest.fixture(params=PARAMS, ids=("NoneParams", "EmptyParams", "StateParams"))
+def params(request):
+    return request.param
+
+
+def test_creation(params):
+    mod = MLP(4, 4, 6)
+    mod = torch.jit.script(mod)
+
+    inputs = torch.randn(10, 4)
+    out = mod(inputs, params)
+    assert out.shape == inputs.shape
