f1cfdc067d73834fe8821832cc692b5ae72d91e7
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-18 21:28:59 -0300

    feat(scripts): add option to load agents from checkpoint
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- scripts/debug_stochastic_actor.py
+++ scripts/debug_stochastic_actor.py
@@ -1,11 +1,19 @@
-# pylint:disable=missing-module-docstring
+# pylint:disable=missing-module-docstring,missing-function-docstring,import-outside-toplevel
 from pprint import pprint
+import warnings
 
 import click
 
+from raylab.cli.utils import initialize_raylab
+
 
 @click.command()
-@click.argument("run", type=str)
+@click.argument("agent_name", type=str)
+@click.option(
+    "--checkpoint",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, resolve_path=False),
+    default=None,
+)
 @click.option(
     "--config",
     "-c",
@@ -27,49 +35,213 @@ import click
     type=click.Path(exists=None, file_okay=False, dir_okay=True),
     default="runs/",
 )
-def main(run, config, script, verbose, tensorboard_dir):
+@click.option("--unconstrained/--constrained", default=False)
+@click.option("--eval/--no-eval", default=False)
+@initialize_raylab
+def main(**args):
     """Print stochastic policy statistics given a config."""
-    # pylint:disable=import-outside-toplevel,too-many-locals
-    import os
-    import shutil
-
     import ray
     from torch.utils.tensorboard import SummaryWriter
-    import raylab
-    from raylab.algorithms.registry import ALGORITHMS
-    from raylab.utils.dynamic_import import import_module_from_path
-
-    if os.path.exists(tensorboard_dir):
-        shutil.rmtree(tensorboard_dir)
 
     ray.init()
-    raylab.register_all_environments()
-    module = import_module_from_path(config)
-    config = module.get_config()
-    config["num_workers"] = 0
-    config["module"]["torch_script"] = script
+    prepare_tensorboard_dir(args["tensorboard_dir"])
+    agent = get_agent(
+        args["agent_name"],
+        args["config"],
+        args["checkpoint"],
+        args["eval"],
+        args["script"],
+    )
+    unconstrain_if_necessary(agent.get_policy(), args["unconstrained"], args["script"])
 
-    agent_cls = ALGORITHMS[run]()
-    policy = agent_cls(config).get_policy()
-    obs_space, action_space = policy.observation_space, policy.action_space
+    test_init(agent.get_policy(), verbose=args["verbose"])
+    test_sampler(agent.get_policy())
+    writer = SummaryWriter(log_dir=args["tensorboard_dir"])
+    rollout = produce_rollout(agent)
+    test_rollout(agent.get_policy(), rollout, writer)
 
-    print("============================= TEST INIT ===================================")
-    test_init(policy, obs_space, action_space, verbose=verbose)
-    print("============================= TEST SAMPLER ================================")
-    test_sampler(policy, obs_space)
-
-    writer = SummaryWriter(log_dir=tensorboard_dir)
-    print("============================= TEST BATCH ==================================")
-    test_big_batch(policy, obs_space, writer)
-    # print("============================= PLOT GRAPH ================================")
+    # print(" PLOT GRAPH ".center(80, "="))
     # if not script:
     #     tensorboard_graph(policy, obs_space, writer)
     writer.close()
 
 
+def get_agent(agent_name, config_path, checkpoint, evaluate, script):
+    from ray.rllib.utils import merge_dicts
+    from raylab.utils.checkpoints import get_config_from_checkpoint, get_agent_cls
+    from raylab.utils.dynamic_import import import_module_from_path
+
+    msg = "Either config or checkpoint can be chosen."
+    assert (config_path is None) != (checkpoint is None), msg
+
+    if config_path is not None:
+        config = import_module_from_path(config_path).get_config()
+        if evaluate:
+            if "evaluation_config" not in config:
+                warnings.warn("Evaluation agent requested but none in config.")
+            else:
+                eval_conf = config["evaluation_config"]
+                config = merge_dicts(config, eval_conf)
+    else:
+
+        config = get_config_from_checkpoint(checkpoint, evaluate)
+
+    config["num_workers"] = 0
+    config["module"]["torch_script"] = script
+    agent_cls = get_agent_cls(agent_name)
+    agent = agent_cls(config)
+    if checkpoint:
+        agent.restore(checkpoint)
+    return agent
+
+
+def prepare_tensorboard_dir(tensorboard_dir):
+    import os
+    import shutil
+
+    if os.path.exists(tensorboard_dir):
+        shutil.rmtree(tensorboard_dir)
+
+
+def unconstrain_if_necessary(policy, unconstrained, script):
+    assert not unconstrained or not script
+    if unconstrained:
+        from raylab.modules.distributions import CompositeTransform
+
+        actor = policy.module.actor
+        transform = actor.dist.transform
+        # Assume transform is composite and squash is last
+        actor.dist.transform = CompositeTransform(transform.transforms[:-1])
+
+
+def test_init(policy, verbose=False):
+    print(" TEST INIT ".center(80, "="))
+    config = policy.config
+    if verbose:
+        pprint(config, indent=2)
+    obs_space, action_space = policy.observation_space, policy.action_space
+    module = policy.module
+
+    print("obs_space".ljust(12), "|", obs_space.low, obs_space.high)
+    print("action_space".ljust(12), "|", action_space.low, action_space.high)
+    print("Module".ljust(12), "|", type(module))
+    print("Parameters".ljust(12), "|", sum(p.numel() for p in module.parameters()))
+
+
+def test_sampler(policy):
+    # pylint:disable=no-member,invalid-name
+    print(" TEST SAMPLER ".center(80, "="))
+    import torch
+
+    module = policy.module
+    obs_space = policy.observation_space
+
+    obs = policy.convert_to_tensor([obs_space.sample()])
+    act, logp = module.actor.rsample(obs)
+    act.detach_()
+    print("OBSERVATION".ljust(12), "|", obs)
+    print("RSAMPLE".ljust(12), "|", act)
+    print("RSAMPLE_LOGP".ljust(12), "|", logp)
+    print("MAX_ACT".ljust(12), "|", act.abs().max())
+
+    print("-" * 60)
+    logp_ = module.actor.log_prob(obs, act)
+    print("EXT_LOGP".ljust(12), "|", logp_)
+
+    print("-" * 60)
+    z, log_det = module.actor.dist.transform(act, module.actor(obs), reverse=True)
+    print("Z".ljust(12), "|", z)
+    print("LOG_DET".ljust(12), "|", log_det)
+    print(
+        "LOGP_Z".ljust(12),
+        "|",
+        module.actor.dist.base_dist.log_prob(z, module.actor(obs)),
+    )
+    act_, logp_ = module.actor.reproduce(obs, act)
+    print("REPR_ACT".ljust(12), "|", act_)
+    print("REPR_LOGP".ljust(12), "|", logp_)
+
+    print("-" * 60)
+    sample_shape = (2,)
+    print("SAMP_SHAPE".ljust(12), "|", sample_shape)
+    act, logp = module.actor.sample(obs, sample_shape)
+    logp.mean().backward()
+    print("SAMPLE".ljust(12), "|", act)
+    print("SAMPLE_LOGP".ljust(12), "|", logp)
+    print("EXT_LOGP".ljust(12), "|", module.actor.log_prob(obs, act))
+    print(
+        "EXT_NORM".ljust(12),
+        "|",
+        torch.cat([p.grad.reshape((-1,)) for p in module.actor.parameters()]).norm(),
+    )
+
+
+def produce_rollout(agent):
+    # pylint:disable=protected-access
+    print(" PRODUCE ROLLOUT ".center(80, "="))
+    import logging
+
+    logging.getLogger("ray.rllib").setLevel(logging.DEBUG)
+    worker = agent.workers.local_worker()
+    worker.batch_mode = "complete_episodes"
+    worker.rollout_fragment_length = 4000
+
+    episode = worker.sample()
+    return agent.get_policy()._lazy_tensor_dict(episode)
+
+
+def test_rollout(policy, rollout, writer):
+    # pylint:disable=no-member,too-many-locals
+    print(" TEST ROLLOUT ".center(80, "="))
+    import torch
+    from ray.rllib.policy.policy import ACTION_LOGP
+    from ray.rllib.policy.sample_batch import SampleBatch
+    from raylab.utils.pytorch import flat_grad
+
+    module = policy.module
+    params = list(module.actor.parameters())
+
+    obs = rollout[SampleBatch.CUR_OBS]
+    acts, logp = rollout[SampleBatch.ACTIONS], rollout[ACTION_LOGP]
+    for idx in range(acts.size(-1)):
+        writer.add_histogram(f"Actions/{idx}", acts[..., idx], 0)
+    writer.add_histogram("Log Probs", logp, 0)
+
+    idxs = torch.randperm(obs.size(0))[:10]
+    print(" SAMPLES ".center(60, "-"))
+    print("Acts".ljust(16), acts[idxs])
+    print("Shape".ljust(16), acts.shape)
+    print("Abs Max".ljust(16), acts.abs().max())
+    print("Mean".ljust(16), acts.mean())
+    print("Logp".ljust(16), logp[idxs])
+    isnan = torch.isnan(logp)
+    print("NaNs".ljust(16), isnan.sum())
+
+    logp_ex = module.actor.log_prob(obs, acts)
+    isnan = torch.isnan(logp_ex)
+    print(" EXT_LOGP ".center(60, "-"))
+    print(logp_ex[idxs])
+    print("Shape".ljust(16), logp_ex.shape)
+    print("NaNs".ljust(16), isnan.sum())
+
+    print()
+    print("MAX(BATCH_LOPG - BATCH_EXT_LOGP): ", (logp - logp_ex).max())
+    print("MEAN(BATCH_LOPG - BATCH_EXT_LOGP):", (logp - logp_ex).mean())
+
+    entropy = -logp_ex.mean()
+    print()
+    print("ENTROPY:", entropy)
+
+    acts = rollout[SampleBatch.ACTIONS]
+    advs = torch.randn_like(obs[..., 0])
+    pol_grad = flat_grad(-(module.actor.log_prob(obs, acts) * advs).mean(), params)
+    print("pol_grad:", pol_grad)
+    print("grad_norm(pg):", pol_grad.norm(p=2))
+
+
 def tensorboard_graph(policy, obs_space, writer):
-    # pylint:disable=missing-docstring,import-outside-toplevel,arguments-differ
-    # pylint:disable=c-extension-no-member,protected-access,too-many-locals
+    # pylint:disable=arguments-differ,too-many-locals
+    # pylint:disable=c-extension-no-member,protected-access
     import torch
     import torch.nn as nn
     from torch.utils.tensorboard._pytorch_graph import (
@@ -82,6 +254,7 @@ def tensorboard_graph(policy, obs_space, writer):
     )
 
     class FullActor(nn.Module):
+        # pylint:disable=missing-class-docstring
         def __init__(self, actor):
             super().__init__()
             self.actor = actor
@@ -103,96 +276,6 @@ def tensorboard_graph(policy, obs_space, writer):
     )
     graphdef = GraphDef(node=list_of_nodes, versions=VersionDef(producer=22)), stepstats
     writer._get_file_writer().add_graph(graphdef)
-
-
-def test_init(policy, obs_space, action_space, verbose=False):
-    # pylint:disable=missing-function-docstring
-    config = policy.config
-    if verbose:
-        pprint(config, indent=2)
-    print("obs_space:", obs_space.low, obs_space.high)
-    print("action_space:", action_space.low, action_space.high)
-    module = policy.module
-
-    print("Module:", type(module))
-    print("Parameters:", sum(p.numel() for p in module.parameters()))
-
-
-def test_sampler(policy, obs_space):
-    # pylint:disable=import-outside-toplevel,no-member,missing-function-docstring
-    # pylint:disable=invalid-name
-    import torch
-
-    module = policy.module
-
-    obs = policy.convert_to_tensor([obs_space.sample()])
-    act, logp = module.actor.rsample(obs)
-    act.detach_()
-    print("OBSERVATION:", obs)
-    print("RSAMPLE:", act)
-    print("LOG_PROB:", logp)
-    print("MAX_ACT:", act.abs().max())
-
-    print("=" * 80)
-    logp_ = module.actor.log_prob(obs, act)
-    print("EXT_LOGP:", logp_)
-
-    print("=" * 80)
-    z, log_det = module.actor.dist.transform(act, module.actor(obs), reverse=True)
-    print("Z:", z)
-    print("LOG_DET:", log_det)
-    print("LOG_PROB_Z:", module.actor.dist.base_dist.log_prob(z, module.actor(obs)))
-    print("REPRODUCED:", module.actor.reproduce(obs, act))
-
-    print("=" * 80)
-    act, logp = module.actor.sample(obs, (2,))
-    logp.mean().backward()
-    print("SAMPLE:", act)
-    print("SAMPLE_LOGP:", logp)
-    print("BATCH_EXT_LOGP:", module.actor.log_prob(obs, act))
-    print(
-        "LOGP_PROB_NORM:",
-        torch.cat([p.grad.reshape((-1,)) for p in module.actor.parameters()]).norm(),
-    )
-
-
-def test_big_batch(policy, obs_space, writer):
-    # pylint:disable=import-outside-toplevel,missing-function-docstring,no-member
-    # pylint:disable=too-many-locals
-    import numpy as np
-    import torch
-    from raylab.utils.pytorch import flat_grad
-
-    module = policy.module
-    params = list(module.actor.parameters())
-
-    obs = policy.convert_to_tensor(np.stack([obs_space.sample() for _ in range(4000)]))
-    acts, logp = module.actor.sample(obs)
-    for idx in range(acts.size(-1)):
-        writer.add_histogram(f"Actions/{idx}", acts[..., idx], 0)
-    writer.add_histogram("Log Probs", logp, 0)
-
-    print("BATCHED_SAMPLES:", acts.shape)
-    print("                ", acts.abs().max())
-    print("BATCHED_LOGP:", logp)
-    isnan = torch.isnan(logp)
-    print("NAN_LOGPs:", logp[isnan], isnan.sum())
-
-    logp_ex = module.actor.log_prob(obs, acts)
-    isnan = torch.isnan(logp_ex)
-    print("BATCH_EXT_LOGP:", logp_ex)
-    print("               ", logp_ex.shape)
-    print("               ", isnan.sum())
-
-    print("MAX(BATCH_LOPG - BATCH_EXT_LOGP):", (logp - logp_ex).max())
-
-    entropy = -logp_ex.mean()
-    print("ENTROPY:", entropy)
-
-    advs = torch.randn_like(obs[..., 0])
-    pol_grad = flat_grad(-(module.actor.log_prob(obs, acts) * advs).mean(), params)
-    print("pol_grad:", pol_grad)
-    print("grad_norm(pg):", pol_grad.norm(p=2))
 
 
 if __name__ == "__main__":
