56200a13cb79a827aa1a00a2e0da0f69170ce805
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-26 11:23:19 -0300

    feat(examples): add reference results for ACKTR on HalfCheetah-v2
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/ACKTR/mujoco_cheetah.py
+++ examples/ACKTR/mujoco_cheetah.py
@@ -3,6 +3,7 @@ from ray import tune
 
 
 def get_config():
+    """Reference results: 1381 return in 1M timesteps."""
     return {
         "env": "HalfCheetah-v2",
         "env_config": {"max_episode_steps": 1000, "time_aware": True},
