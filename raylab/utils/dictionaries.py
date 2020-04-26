bf5e1fff538486821430b4a6318581e126d887ac
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-26 12:30:29 -0300

    feat(utils): add all_except
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/utils/dictionaries.py
+++ raylab/utils/dictionaries.py
@@ -13,3 +13,12 @@ def deep_merge(dict1, dict2, *args, **kwargs):
 def get_keys(mapping, *keys):
     """Return the values corresponding to the given keys, in order."""
     return (mapping[k] for k in keys)
+
+
+def all_except(mapping, *exclude):
+    """Return a new mapping with all keys except `exclude`.
+
+    Keys must be hashable to be used with `set`.
+    """
+    exclude = set(exclude)
+    return {k: v for k, v in mapping.items() if k not in exclude}
