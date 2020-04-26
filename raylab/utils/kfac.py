e81f2e917666d225a4440987265db6bf4441ebd4
Ângelo Lovatto <angelolovatto@gmail.com>
2020-04-25 22:34:09 -0300

    refactor(utils): streamline KFACOptimizer
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/utils/kfac.py
+++ raylab/utils/kfac.py
@@ -68,8 +68,8 @@ class KFACOptimizer(Optimizer):
         for mod in net.modules():
             mod_class = type(mod).__name__
             if mod_class in ["Linear", "Conv2d"]:
-                self._fwd_handles += [mod.register_forward_pre_hook(self._save_input)]
-                self._bwd_handles += [mod.register_backward_hook(self._save_grad_out)]
+                self._fwd_handles += [mod.register_forward_pre_hook(self.save_input)]
+                self._bwd_handles += [mod.register_backward_hook(self.save_grad_out)]
                 info = (
                     (mod.kernel_size, mod.padding, mod.stride)
                     if mod_class == "Conv2d"
@@ -89,6 +89,29 @@ class KFACOptimizer(Optimizer):
         super(KFACOptimizer, self).__init__(param_groups, {"lr": lr})
         self.state["kl_clip"] = kl_clip
 
+    @contextlib.contextmanager
+    def record_stats(self):
+        """Activate registered forward and backward hooks."""
+        try:
+            self._recording = True
+            yield
+        except Exception as excep:
+            raise excep
+        finally:
+            self._recording = False
+
+    def save_input(self, mod, inputs):
+        """Saves input of layer to compute covariance."""
+        if self._recording:
+            self.state[mod.weight]["x"] = inputs[0].detach()
+
+    def save_grad_out(self, mod, _, grad_outputs):
+        """Saves grad on output of layer to compute covariance."""
+        if self._recording:
+            self.state[mod.weight]["gy"] = (
+                grad_outputs[0] * grad_outputs[0].size(0)
+            ).detach()
+
     def step(self):  # pylint:disable=arguments-differ
         """Preconditions and applies gradients."""
         fisher_norm = 0.0
@@ -100,12 +123,9 @@ class KFACOptimizer(Optimizer):
 
             # Update convariances and inverses
             state.setdefault("step", 0)
-            self._compute_covs(group, state)
+            state.update(self._compute_covs(group, state))
             if state["step"] % self.update_freq == 0:
-                ixxt, iggt = self._inv_covs(
-                    state["xxt"], state["ggt"], state["num_locations"]
-                )
-                state.update((("ixxt", ixxt), ("iggt", iggt)))
+                state.update(self._process_covs(state))
             state["step"] += 1
 
             # Preconditionning
@@ -132,54 +152,92 @@ class KFACOptimizer(Optimizer):
                 param.grad.data.mul_(scale)
                 param.data.sub_(group["lr"], param.grad.data)
 
-    @contextlib.contextmanager
-    def record_stats(self):
-        """Activate registered forward and backward hooks."""
-        try:
-            self._recording = True
-            yield
-        except Exception as excep:
-            raise excep
-        finally:
-            self._recording = False
+    def _compute_covs(self, group, state):
+        """Computes the covariances."""
+        x, gy = state["x"], state["gy"]
+        # Computation of xxt
+        if group["layer_type"] == "Conv2d":
+            if not self.sua:
+                kernel_size, padding, stride = group["info"]
+                x = F.unfold(x, kernel_size, padding=padding, stride=stride)
+            else:
+                x = x.view(x.shape[0], x.shape[1], -1)
+            x = x.data.permute(1, 0, 2).reshape(x.shape[1], -1)
+        else:
+            x = x.data.t()
+        batch_size = x.shape[1]
 
-    def _save_input(self, mod, i):
-        """Saves input of layer to compute covariance."""
-        if self._recording:
-            self.state[mod.weight]["x"] = i[0]
+        if len(group["params"]) == 2:
+            ones = torch.ones_like(x[:1])
+            x = torch.cat([x, ones], dim=0)
 
-    def _save_grad_out(self, mod, _, grad_output):
-        """Saves grad on output of layer to compute covariance."""
-        if self._recording:
-            self.state[mod.weight]["gy"] = grad_output[0] * grad_output[0].size(0)
+        # Computation of xxt
+        xxt = torch.mm(x, x.t()) / batch_size
+        if "xxt" in state:
+            xxt = state["xxt"] * (1.0 - self.alpha) + xxt * self.alpha
+
+        # Computation of ggt
+        if group["layer_type"] == "Conv2d":
+            gy = gy.data.permute(1, 0, 2, 3)
+            num_locations = gy.shape[2] * gy.shape[3]
+            gy = gy.reshape(gy.shape[0], -1)
+        else:
+            gy = gy.data.t()
+            num_locations = 1
+
+        ggt = torch.mm(gy, gy.t()) / batch_size
+        if "ggt" in state:
+            ggt = state["ggt"] * (1.0 - self.alpha) + ggt * self.alpha
+
+        return {"xxt": xxt, "ggt": ggt, "num_locations": num_locations}
+
+    def _process_covs(self, state):
+        """Process the covariances for preconditioning gradients later."""
+        xxt, ggt, num_locations = (state[k] for k in "xxt ggt num_locations".split())
+        # Computes pi
+        pi = 1.0
+        if self.pi:
+            pi = (torch.trace(xxt) * ggt.shape[0]) / (torch.trace(ggt) * xxt.shape[0])
+
+        # Regularizes and inverts
+        eps = self.eps / num_locations
+        diag_xxt = torch.diag(torch.empty(xxt.shape[0]).fill_(torch.sqrt(eps * pi)))
+        diag_ggt = torch.diag(torch.empty(ggt.shape[0]).fill_(torch.sqrt(eps / pi)))
+        # sa, ua = torch.symeig(xxt + diag_xxt, eigenvectors=True)
+        # sb, ub = torch.symeig(ggt + diag_ggt, eigenvectors=True)
+        # return {"ua": ua, "sa": sa, "ub": ub, "sb": sb}
+        ixxt = (xxt + diag_xxt).inverse()
+        iggt = (ggt + diag_ggt).inverse()
+        return {"ixxt": ixxt, "iggt": iggt}
 
     def _precond(self, weight, bias, group, state):
         """Applies preconditioning."""
         if group["layer_type"] == "Conv2d" and self.sua:
             return self._precond_sua(weight, bias, state)
 
-        ixxt = state["ixxt"]
-        iggt = state["iggt"]
         g = weight.grad.data
-        s = g.shape
         if group["layer_type"] == "Conv2d":
-            g = g.contiguous().view(s[0], s[1] * s[2] * s[3])
-
+            g = g.reshape(g.shape[0], -1)
         if bias is not None:
             gb = bias.grad.data
             g = torch.cat([g, gb.view(gb.shape[0], 1)], dim=1)
 
-        g = torch.mm(torch.mm(iggt, g), ixxt)
+        # ua, sa, ub, sb = (state[k] for k in "ua sa ub sb".split())
+        # projected = ub @ g @ ua.T
+        # scaled = torch.diag(1 / sb) @ projected @ torch.diag(1 / sa)
+        # g = ub.T @ scaled @ ua
+        ixxt, iggt = state["ixxt"], state["iggt"]
+        g = iggt @ g @ ixxt
         if group["layer_type"] == "Conv2d":
             g /= state["num_locations"]
 
         if bias is not None:
-            gb = g[:, -1].contiguous().view(*bias.shape)
+            gb = g[:, -1].reshape_as(bias)
             g = g[:, :-1]
         else:
             gb = None
 
-        g = g.contiguous().view(*s)
+        g = g.reshape_as(weight)
         return g, gb
 
     @staticmethod
@@ -205,70 +263,6 @@ class KFACOptimizer(Optimizer):
             gb = None
 
         return g, gb
-
-    def _compute_covs(self, group, state):
-        """Computes the covariances."""
-        x, gy = state["x"], state["gy"]
-        # Computation of xxt
-        if group["layer_type"] == "Conv2d":
-            if not self.sua:
-                kernel_size, padding, stride = group["info"]
-                x = F.unfold(x, kernel_size, padding=padding, stride=stride)
-            else:
-                x = x.view(x.shape[0], x.shape[1], -1)
-            x = x.data.permute(1, 0, 2).contiguous().view(x.shape[1], -1)
-        else:
-            x = x.data.t()
-
-        if len(group["params"]) == 2:
-            ones = torch.ones_like(x[:1])
-            x = torch.cat([x, ones], dim=0)
-
-        if state["step"] == 0:
-            state["xxt"] = torch.mm(x, x.t()) / float(x.shape[1])
-        else:
-            state["xxt"].addmm_(
-                mat1=x,
-                mat2=x.t(),
-                beta=(1.0 - self.alpha),
-                alpha=self.alpha / float(x.shape[1]),
-            )
-
-        # Computation of ggt
-        if group["layer_type"] == "Conv2d":
-            gy = gy.data.permute(1, 0, 2, 3)
-            state["num_locations"] = gy.shape[2] * gy.shape[3]
-            gy = gy.contiguous().view(gy.shape[0], -1)
-        else:
-            gy = gy.data.t()
-            state["num_locations"] = 1
-
-        if state["step"] == 0:
-            state["ggt"] = torch.mm(gy, gy.t()) / float(gy.shape[1])
-        else:
-            state["ggt"].addmm_(
-                mat1=gy,
-                mat2=gy.t(),
-                beta=(1.0 - self.alpha),
-                alpha=self.alpha / float(gy.shape[1]),
-            )
-
-    def _inv_covs(self, xxt, ggt, num_locations):
-        """Inverts the covariances."""
-        # Computes pi
-        pi = 1.0
-        if self.pi:
-            tx = torch.trace(xxt) * ggt.shape[0]
-            tg = torch.trace(ggt) * xxt.shape[0]
-            pi = tx / tg
-
-        # Regularizes and inverts
-        eps = self.eps / num_locations
-        diag_xxt = xxt.new(xxt.shape[0]).fill_((eps * pi) ** 0.5)
-        diag_ggt = ggt.new(ggt.shape[0]).fill_((eps / pi) ** 0.5)
-        ixxt = (xxt + torch.diag(diag_xxt)).inverse()
-        iggt = (ggt + torch.diag(diag_ggt)).inverse()
-        return ixxt, iggt
 
     def __del__(self):
         for handle in self._fwd_handles + self._bwd_handles:
