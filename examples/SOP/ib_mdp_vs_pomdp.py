b4e21589e2a30225be9103b0c49f41beb19b052e
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-01 08:02:06 -0300

    chore(examples): update configurations for SOP
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/SOP/ib_mdp_vs_pomdp.py
+++ examples/SOP/ib_mdp_vs_pomdp.py
@@ -1,18 +1,21 @@
-"""Tune experiment configuration to test SOP in the Industrial Benchmark."""
 from ray import tune
+from ray.rllib.utils import merge_dicts
+
 from ib_defaults import get_config as base_config
 
 
-def get_config():  # pylint: disable=missing-docstring
-    return {
-        **base_config(),
-        # === Environment ===
-        "env": "IndustrialBenchmark",
-        "env_config": {
-            "reward_type": "classic",
-            "action_type": "continuous",
-            "observation": tune.grid_search(["visible", "markovian"]),
-            "max_episode_steps": 1000,
-            "time_aware": True,
+def get_config():
+    return merge_dicts(
+        base_config(),
+        {
+            # === Environment ===
+            "env": "IndustrialBenchmark",
+            "env_config": {
+                "reward_type": "classic",
+                "action_type": "continuous",
+                "observation": tune.grid_search(["visible", "markovian"]),
+                "max_episode_steps": 1000,
+                "time_aware": True,
+            },
         },
-    }
+    )
--- examples/SOP/swingup_defaults.py
+++ examples/SOP/swingup_defaults.py
@@ -1,33 +1,22 @@
-"""Tune experiment configuration to test SOP in CartPoleSwingUp."""
-from ray import tune
-
-
 def get_config():
     return {
         # === Environment ===
         "env": "CartPoleSwingUp",
         "env_config": {"max_episode_steps": 500, "time_aware": False},
-        # === SQUASHING EXPLORATION PROBLEM ===
-        # Maximum l1 norm of the policy's output vector before the squashing function
-        "beta": 1.2,
         # === Twin Delayed DDPG (TD3) tricks ===
         # Clipped Double Q-Learning: use the minimun of two target Q functions
         # as the next action-value in the target for fitted Q iteration
         "clipped_double_q": True,
-        # Add gaussian noise to the action when calculating the Deterministic
-        # Policy Gradient
-        "target_policy_smoothing": True,
-        # Additive Gaussian i.i.d. noise to add to actions inputs to target Q function
-        "target_gaussian_sigma": 0.2,
         # === Replay buffer ===
         # Size of the replay buffer. Note that if async_updates is set, then
         # each worker will have a replay buffer of this size.
         "buffer_size": int(1e5),
         # === Optimization ===
-        # PyTorch optimizer to use for policy
-        "policy_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
-        # PyTorch optimizer to use for critic
-        "critic_optimizer": {"name": "Adam", "options": {"lr": 3e-4}},
+        # PyTorch optimizers to use
+        "torch_optimizer": {
+            "actor": {"type": "Adam", "lr": 3e-4},
+            "critics": {"type": "Adam", "lr": 3e-4},
+        },
         # Interpolation factor in polyak averaging for target networks.
         "polyak": 0.995,
         # === Network ===
@@ -35,40 +24,35 @@ def get_config():
         # for the policy and action-value function. No layers means the component is
         # linear in states and/or actions.
         "module": {
-            "policy": {
-                "units": (128, 128),
-                "activation": "ReLU",
-                "initializer_options": {"name": "xavier_uniform"},
+            "type": "DDPGModule",
+            "actor": {
+                "smooth_target_policy": True,
+                "target_gaussian_sigma": 0.2,
+                "beta": 1.2,
+                "encoder": {
+                    "units": (128, 128),
+                    "activation": "ReLU",
+                    "initializer_options": {"name": "xavier_uniform"},
+                    "layer_norm": False,
+                },
             },
             "critic": {
-                "units": (128, 128),
-                "activation": "ReLU",
-                "initializer_options": {"name": "xavier_uniform"},
-                "delay_action": True,
+                "double_q": False,
+                "encoder": {
+                    "units": (128, 128),
+                    "activation": "ReLU",
+                    "initializer_options": {"name": "xavier_uniform"},
+                    "delay_action": True,
+                },
             },
         },
-        # === Exploration ===
-        # Which type of exploration to use. Possible types include
-        # None: use the greedy policy to act
-        # parameter_noise: use parameter space noise
-        # gaussian: use i.i.d gaussian action space noise independently for each
-        #     action dimension
-        "exploration": "gaussian",
-        # Options for parameter noise exploration
-        "param_noise_spec": {
-            "initial_stddev": 0.1,
-            "desired_action_stddev": 0.3,
-            "adaptation_coeff": 1.01,
+        # === Exploration Settings ===
+        # Provide a dict specifying the Exploration object's config.
+        "exploration_config": {
+            "type": "raylab.utils.exploration.GaussianNoise",
+            "noise_stddev": 0.3,
+            "pure_exploration_steps": 5000,
         },
-        # Additive Gaussian i.i.d. noise to add to actions before squashing
-        "exploration_gaussian_sigma": 0.3,
-        # Until this many timesteps have elapsed, the agent's policy will be
-        # ignored & it will instead take uniform random actions. Can be used in
-        # conjunction with learning_starts (which controls when the first
-        # optimization step happens) to decrease dependence of exploration &
-        # optimization on initial policy parameters. Note that this will be
-        # disabled when the action noise scale is set to 0 (e.g during evaluation).
-        "pure_exploration_steps": 5000,
         # === Trainer ===
         "train_batch_size": 128,
         "timesteps_per_iteration": 1000,
@@ -78,10 +62,4 @@ def get_config():
         "evaluation_interval": 5,
         # Number of episodes to run per evaluation period.
         "evaluation_num_episodes": 5,
-        # === Debugging ===
-        # Set the ray.rllib.* log level for the agent process and its workers.
-        # Should be one of DEBUG, INFO, WARN, or ERROR. The DEBUG level will also
-        # periodically print out summaries of relevant internal dataflow (this is
-        # also printed out once at startup at the INFO level).
-        "log_level": "WARN",
     }
