37da0d85701438a84ecae49ddb912a4ef8586edd
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-01 08:34:04 -0300

    chore(examples): update configurations for SVGMaxEnt
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/SVGMaxEnt/nfmbrl_ib.py
+++ examples/SVGMaxEnt/nfmbrl_ib.py
@@ -74,39 +74,13 @@ def get_config():
         # for the policy, value function and model. No layers means the component is
         # linear in states and/or actions.
         "module": tune.grid_search([GAUSSIAN, FLOW]),
-        # "module": GAUSSIAN,
-        # "module": FLOW,
         # === Trainer ===
         "train_batch_size": 256,
         "timesteps_per_iteration": 1000,
         # === Exploration Settings ===
-        # Default exploration behavior, iff `explore`=None is passed into
-        # compute_action(s).
-        # Set to False for no exploration behavior (e.g., for evaluation).
-        "explore": True,
-        # Provide a dict specifying the Exploration object's config.
-        "exploration_config": {
-            # The Exploration class to use. In the simplest case, this is the name
-            # (str) of any class present in the `rllib.utils.exploration` package.
-            # You can also provide the python class directly or the full location
-            # of your class (e.g. "ray.rllib.utils.exploration.epsilon_greedy.
-            # EpsilonGreedy").
-            "type": "raylab.utils.exploration.StochasticActor",
-            # Options for parameter noise exploration
-            # Until this many timesteps have elapsed, the agent's policy will be
-            # ignored & it will instead take uniform random actions. Can be used in
-            # conjunction with learning_starts (which controls when the first
-            # optimization step happens) to decrease dependence of exploration &
-            # optimization on initial policy parameters. Note that this will be
-            # disabled when the action noise scale is set to 0 (e.g during evaluation).
-            "pure_exploration_steps": 10000,
-        },
+        "exploration_config": {"pure_exploration_steps": 10000},
         # === Evaluation ===
         # Evaluate with every `evaluation_interval` training iterations.
         # The evaluation stats will be reported under the "evaluation" metric key.
         "evaluation_interval": 5,
-        # Extra arguments to pass to evaluation workers.
-        # Typical usage is to pass extra args to evaluation env creator
-        # and to disable exploration by computing deterministic actions
-        "evaluation_config": {"explore": False},
     }
--- examples/SVGMaxEnt/reacher_defaults.py
+++ examples/SVGMaxEnt/reacher_defaults.py
@@ -4,7 +4,7 @@ from ray import tune
 def get_config():
     return {
         # === Environment ===
-        "env": "MujocoReacher",
+        "env": "Reacher-v2",
         "env_config": {"max_episode_steps": 50, "time_aware": True},
         # === Optimization ===
         # PyTorch optimizers to use
@@ -36,27 +36,7 @@ def get_config():
         "train_batch_size": 128,
         "timesteps_per_iteration": 1000,
         # === Exploration Settings ===
-        # Default exploration behavior, iff `explore`=None is passed into
-        # compute_action(s).
-        # Set to False for no exploration behavior (e.g., for evaluation).
-        "explore": True,
-        # Provide a dict specifying the Exploration object's config.
-        "exploration_config": {
-            # The Exploration class to use. In the simplest case, this is the name
-            # (str) of any class present in the `rllib.utils.exploration` package.
-            # You can also provide the python class directly or the full location
-            # of your class (e.g. "ray.rllib.utils.exploration.epsilon_greedy.
-            # EpsilonGreedy").
-            "type": "raylab.utils.exploration.StochasticActor",
-            # Options for parameter noise exploration
-            # Until this many timesteps have elapsed, the agent's policy will be
-            # ignored & it will instead take uniform random actions. Can be used in
-            # conjunction with learning_starts (which controls when the first
-            # optimization step happens) to decrease dependence of exploration &
-            # optimization on initial policy parameters. Note that this will be
-            # disabled when the action noise scale is set to 0 (e.g during evaluation).
-            "pure_exploration_steps": 500,
-        },
+        "exploration_config": {"pure_exploration_steps": 500},
         # === Evaluation ===
         # Evaluate with every `evaluation_interval` training iterations.
         # The evaluation stats will be reported under the "evaluation" metric key.
