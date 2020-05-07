e29e06ce38b4bed3b21b1675c535cadfc0acbb52
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-07 10:00:25 -0300

    chore: update setup.py
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
 # pylint: disable=missing-docstring
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
 
 setup(
     name="raylab",
     version="0.6.2",
-    py_modules=["raylab"],
+    author="Ângelo G. Lovatto",
+    author_email="angelolovatto@gmail.com",
+    description="Reinforcement learning algorithms in RLlib and PyTorch.",
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/angelolovatto/raylab",
+    packages=find_packages(),
     install_requires=[
+        "bokeh",
         "Click",
-        "matplotlib",
         "pandas",
-        "seaborn",
         "requests",
         "ray[rllib]",
+        "streamlit",
+        "torch",
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
     entry_points="""
         [console_scripts]
         raylab=raylab.cli:cli
