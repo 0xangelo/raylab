fix(mapo): check model with numerically stable obs and actions
Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

raylab/agents/mapo/mapo_policy.py
@@ -46,7 +46,11 @@ class MAPOTorchPolicy(raypi.TargetNetworksMixin, raypi.TorchPolicy):
         # pylint:disable=no-member
         module = super().make_module(obs_space, action_space, config)
         # pylint:enable=no-member
-        self.check_model(module.model.rsample)
+        self.check_model(
+            module.model.rsample
+            if config["grad_estimator"] == "PD"
+            else module.model.sample
+        )
         return module
 
     @override(raypi.TorchPolicy)
@@ -73,9 +77,9 @@ class MAPOTorchPolicy(raypi.TargetNetworksMixin, raypi.TorchPolicy):
 
     def check_model(self, sampler):
         """Verify that the transition model is appropriate for the desired estimator."""
+        obs = torch.randn(self.observation_space.shape)[None]
+        act = torch.randn(self.action_space.shape)[None]
         if self.config["grad_estimator"] == "SF":
-            obs = self.convert_to_tensor(self.observation_space.sample())[None]
-            act = self.convert_to_tensor(self.action_space.sample())[None]
             sample, logp = sampler(obs, act.requires_grad_())
             assert sample.grad_fn is None
             assert logp is not None
@@ -85,10 +89,8 @@ class MAPOTorchPolicy(raypi.TargetNetworksMixin, raypi.TorchPolicy):
             ), "Transition grad log_prob must exist for SF estimator"
             assert not torch.allclose(act.grad, torch.zeros_like(act))
         if self.config["grad_estimator"] == "PD":
-            obs = self.convert_to_tensor(self.observation_space.sample())[None]
-            act = self.convert_to_tensor(self.action_space.sample())[None]
-            samp, _ = sampler(obs.requires_grad_(), act.requires_grad_())
-            samp.mean().backward()
+            sample, _ = sampler(obs.requires_grad_(), act.requires_grad_())
+            sample.mean().backward()
             assert (
                 obs.grad is not None and act.grad is not None
             ), "Transition grad w.r.t. state and action must exist for PD estimator"