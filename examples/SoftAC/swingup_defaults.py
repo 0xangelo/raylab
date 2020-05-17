01b0f90e7310f1f18fa64f8509d1cbbce51439fd
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-17 15:20:42 -0300

    chore(examples): add uniform exploration for SoftAC on SwingUp
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/SoftAC/swingup_defaults.py
+++ examples/SoftAC/swingup_defaults.py
@@ -5,7 +5,7 @@ def get_config():
     return {
         # === Environment ===
         "env": "CartPoleSwingUp-v1",
-        "env_config": {"max_episode_steps": 500, "time_aware": False},
+        "env_config": {"max_episode_steps": 200, "time_aware": False},
         # === Replay Buffer ===
         "buffer_size": int(1e5),
         # === Optimization ===
@@ -25,6 +25,7 @@ def get_config():
         "timesteps_per_iteration": 1000,
         # === Exploration Settings ===
         "exploration_config": {"pure_exploration_steps": 5000},
+        "learning_starts": 5000,
         # === Evaluation ===
         # Evaluate with every `evaluation_interval` training iterations.
         # The evaluation stats will be reported under the "evaluation" metric key.
