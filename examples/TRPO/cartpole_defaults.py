48a46f4385837083b6f289a028a33825e5fc634d
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-17 14:30:32 -0300

    chore(examples): fix TRPO options
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- examples/TRPO/cartpole_defaults.py
+++ examples/TRPO/cartpole_defaults.py
@@ -1,39 +1,40 @@
-from ray import tune
-
-
 def get_config():
     return {
         # === Environment ===
-        "env": "CartPole-v0",
-        "env_config": {"max_episode_steps": 200, "time_aware": True},
+        "env": "CartPole-v1",
+        "env_config": {"max_episode_steps": 500, "time_aware": True},
         # Trust region constraint
         "delta": 0.01,
         # Number of actions to sample per state for Fisher vector product approximation
-        "fvp_samples": 20,
+        "fvp_samples": 10,
         # For GAE(\gamma, \lambda)
         "gamma": 0.99,
         "lambda": 0.97,
         # Number of iterations to fit value function
         "val_iters": 40,
-        # Learning rate for critic optimizer
-        "val_lr": 1e-2,
+        # Options for critic optimizer
+        "torch_optimizer": {"type": "Adam", "lr": 1e-2},
         # === RolloutWorker ===
         "num_workers": 2,
         "num_envs_per_worker": 8,
-        "rollout_fragment_length": 400,
+        "rollout_fragment_length": 125,
         "batch_mode": "truncate_episodes",
-        "timesteps_per_iteration": 6400,
+        "timesteps_per_iteration": 2000,
         # === Network ===
         "module": {
             "actor": {
-                "units": (32, 32),
-                "activation": "ELU",
-                "initializer_options": {"name": "orthogonal"},
+                "encoder": {
+                    "units": (64, 64),
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
             },
             "critic": {
-                "units": (32, 32),
-                "activation": "ELU",
-                "initializer_options": {"name": "orthogonal"},
+                "encoder": {
+                    "units": (64, 64),
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
             },
         },
     }
--- examples/TRPO/gaussian_swingup.py
+++ examples/TRPO/gaussian_swingup.py
@@ -9,31 +9,35 @@ def get_config():
         # Trust region constraint
         "delta": 0.01,
         # Number of actions to sample per state for Fisher vector product approximation
-        "fvp_samples": 20,
+        "fvp_samples": 10,
         # For GAE(\gamma, \lambda)
         "gamma": 0.99,
         "lambda": 0.97,
         # Number of iterations to fit value function
         "val_iters": 40,
-        # Learning rate for critic optimizer
-        "val_lr": 1e-2,
+        # Options for critic optimizer
+        "torch_optimizer": {"type": "Adam", "lr": 1e-2},
         # === RolloutWorker ===
         "num_workers": 2,
         "num_envs_per_worker": 8,
-        "rollout_fragment_length": 400,
+        "rollout_fragment_length": 125,
         "batch_mode": "truncate_episodes",
-        "timesteps_per_iteration": 6400,
+        "timesteps_per_iteration": 2000,
         # === Network ===
         "module": {
             "actor": {
-                "units": (32, 32),
-                "activation": "ELU",
-                "initializer_options": {"name": "orthogonal"},
+                "encoder": {
+                    "units": (32, 32),
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
             },
             "critic": {
-                "units": (32, 32),
-                "activation": "ELU",
-                "initializer_options": {"name": "orthogonal"},
+                "encoder": {
+                    "units": (32, 32),
+                    "activation": "ELU",
+                    "initializer_options": {"name": "orthogonal"},
+                },
             },
         },
     }
