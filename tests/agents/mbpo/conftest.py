ba2d76bf3c94e8b3fd0c108e9433f07d7bd215af
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-27 09:36:00 -0300

    test: add tests for MBPO
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ tests/agents/mbpo/conftest.py
@@ -0,0 +1,22 @@
+# pylint: disable=missing-docstring,redefined-outer-name,protected-access
+import pytest
+
+from raylab.agents.registry import AGENTS
+
+
+@pytest.fixture(scope="module")
+def mbpo_trainer():
+    return AGENTS["MBPO"]()
+
+
+@pytest.fixture(scope="module")
+def mbpo_policy(mbpo_trainer):
+    return mbpo_trainer._policy
+
+
+@pytest.fixture(scope="module")
+def policy_and_batch_fn(policy_and_batch_, mbpo_policy):
+    def make_policy_and_batch(config):
+        return policy_and_batch_(mbpo_policy, config)
+
+    return make_policy_and_batch
---
+++ tests/agents/mbpo/test_policy.py
@@ -0,0 +1,21 @@
+# pylint: disable=missing-docstring,redefined-outer-name,protected-access
+import pytest
+
+
+@pytest.fixture(scope="module")
+def config():
+    return {"module": {"ensemble_size": 1}}
+
+
+@pytest.fixture(scope="module")
+def policy(policy_and_batch_fn, config):
+    return policy_and_batch_fn(config)
+
+
+def test_policy_creation(policy):
+    assert "models" in policy.module
+    assert "actor" in policy.module
+    assert "critics" in policy.module
+    assert "alpha" in policy.module
+
+    assert len(policy.optimizer) == 4
