d80002ac75cf53ff5ed5996b933dcbfcfedf76d0
Ângelo Lovatto <angelolovatto@gmail.com>
2020-05-29 09:21:02 -0300

    feat(losses): fix batch keys in MaximumLikelihood losses'
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/losses/mle.py
+++ raylab/losses/mle.py
@@ -9,14 +9,14 @@ from raylab.utils.dictionaries import get_keys
 class MaximumLikelihood:
     """Loss function for model learning of single transitions."""
 
+    batch_keys = (SampleBatch.CUR_OBS, SampleBatch.ACTIONS, SampleBatch.NEXT_OBS)
+
     def __init__(self, model):
         self.model = model
 
     def __call__(self, batch):
         """Compute Maximum Likelihood Estimation (MLE) model loss."""
-        obs, actions, next_obs = get_keys(
-            batch, SampleBatch.CUR_OBS, SampleBatch.ACTIONS, SampleBatch.NEXT_OBS
-        )
+        obs, actions, next_obs = get_keys(batch, self.batch_keys)
         loss = -self.model_likelihood(obs, actions, next_obs).mean()
         return loss, {"loss(mle)": loss.item()}
 
