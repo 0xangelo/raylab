fix(envs): address precision errors with relative timestep
Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

raylab/envs/environments/hvac.py
@@ -80,47 +80,10 @@ class HVACEnv(gym.Env):
         temp_outside, logp_temp_outside = self._temp_outside(sample_shape)
 
         next_state = self._temp(action, temp_outside, temp_hall)
-
-        time = torch.clamp(time + 1 / self._horizon, 0.0, 1.0).detach()
-        time = time.expand_as(next_state[..., -1:])
-
         logp = logp_temp_hall + logp_temp_outside
-
+        time = self._step_time(time)
+        time = time.expand_as(next_state[..., -1:])
         return torch.cat([next_state, time], dim=-1), logp
-
-    def reward_fn(self, state, action, next_state):
-        # pylint: disable=unused-argument,missing-docstring
-        AIR_MAX = torch.as_tensor(self._config["AIR_MAX"])
-        air = torch.as_tensor(action) * AIR_MAX
-
-        temp, _ = self._unpack_state(next_state)
-
-        IS_ROOM = torch.as_tensor(self._config["IS_ROOM"])
-        COST_AIR = torch.as_tensor(self._config["COST_AIR"])
-        TEMP_LOW = torch.as_tensor(self._config["TEMP_LOW"])
-        TEMP_UP = torch.as_tensor(self._config["TEMP_UP"])
-        PENALTY = torch.as_tensor(self._config["PENALTY"])
-
-        reward = -(
-            IS_ROOM
-            * (
-                air * COST_AIR
-                + ((temp < TEMP_LOW) | (temp > TEMP_UP)) * PENALTY
-                + 10.0 * torch.abs((TEMP_UP + TEMP_LOW) / 2.0 - temp)
-            )
-        ).sum(dim=-1)
-
-        return reward
-
-    def _temp_outside(self, sample_shape=()):
-        TEMP_OUTSIDE_MEAN = torch.as_tensor(self._config["TEMP_OUTSIDE_MEAN"])
-        TEMP_OUTSIDE_VARIANCE = torch.sqrt(
-            torch.as_tensor(self._config["TEMP_OUTSIDE_VARIANCE"])
-        )
-        dist = torch.distributions.Normal(TEMP_OUTSIDE_MEAN, TEMP_OUTSIDE_VARIANCE)
-        sample = dist.rsample(sample_shape)
-        logp = dist.log_prob(sample.detach())
-        return sample, logp
 
     def _temp_hall(self, sample_shape=()):
         TEMP_HALL_MEAN = torch.as_tensor(self._config["TEMP_HALL_MEAN"])
@@ -128,6 +91,16 @@ class HVACEnv(gym.Env):
             torch.as_tensor(self._config["TEMP_HALL_VARIANCE"])
         )
         dist = torch.distributions.Normal(TEMP_HALL_MEAN, TEMP_HALL_VARIANCE)
+        sample = dist.rsample(sample_shape)
+        logp = dist.log_prob(sample.detach())
+        return sample, logp
+
+    def _temp_outside(self, sample_shape=()):
+        TEMP_OUTSIDE_MEAN = torch.as_tensor(self._config["TEMP_OUTSIDE_MEAN"])
+        TEMP_OUTSIDE_VARIANCE = torch.sqrt(
+            torch.as_tensor(self._config["TEMP_OUTSIDE_VARIANCE"])
+        )
+        dist = torch.distributions.Normal(TEMP_OUTSIDE_MEAN, TEMP_OUTSIDE_VARIANCE)
         sample = dist.rsample(sample_shape)
         logp = dist.log_prob(sample.detach())
         return sample, logp
@@ -163,15 +136,43 @@ class HVACEnv(gym.Env):
 
         return temp_
 
+    def _step_time(self, time):
+        timestep = torch.round(self._horizon * time)
+        return (timestep + 1) / self._horizon
+
+    def reward_fn(self, state, action, next_state):
+        # pylint: disable=unused-argument,missing-docstring
+        AIR_MAX = torch.as_tensor(self._config["AIR_MAX"])
+        air = torch.as_tensor(action) * AIR_MAX
+
+        temp, _ = self._unpack_state(next_state)
+
+        IS_ROOM = torch.as_tensor(self._config["IS_ROOM"])
+        COST_AIR = torch.as_tensor(self._config["COST_AIR"])
+        TEMP_LOW = torch.as_tensor(self._config["TEMP_LOW"])
+        TEMP_UP = torch.as_tensor(self._config["TEMP_UP"])
+        PENALTY = torch.as_tensor(self._config["PENALTY"])
+
+        reward = -(
+            IS_ROOM
+            * (
+                air * COST_AIR
+                + ((temp < TEMP_LOW) | (temp > TEMP_UP)) * PENALTY
+                + 10.0 * torch.abs((TEMP_UP + TEMP_LOW) / 2.0 - temp)
+            )
+        ).sum(dim=-1)
+
+        return reward
+
     def _terminal(self):
         _, time = self._unpack_state(self._state)
-        return np.allclose(time.numpy(), 1.0)
-
-    def render(self, mode="human"):
-        pass
+        return time.item() >= 1.0
 
     @staticmethod
     def _unpack_state(state):
         obs = torch.as_tensor(state[..., :-1], dtype=torch.float32)
-        time = torch.as_tensor(state[..., -1], dtype=torch.float32)
+        time = torch.as_tensor(state[..., -1:], dtype=torch.float32)
         return obs, time
+
+    def render(self, mode="human"):
+        pass

raylab/envs/environments/navigation.py
@@ -92,24 +92,24 @@ class NavigationEnv(gym.Env):
 
     def transition_fn(self, state, action, sample_shape=()):
         # pylint: disable=missing-docstring
-        state, time = state[..., :2], state[..., 2:]
+        position, time = self._unpack_state(state)
         deceleration = 1.0
         if self._deceleration_zones:
-            deceleration = self._deceleration(state)
+            deceleration = self._deceleration(position)
 
-        position = state + (deceleration * action)
-        next_state, logp = self._sample_noise(position, sample_shape)
-        time = torch.clamp(time + 1 / self._horizon, 0.0, 1.0).detach()
-        time = time.expand_as(next_state[..., -1:])
-        return torch.cat([next_state, time], dim=-1), logp
+        position = position + (deceleration * action)
+        position, logp = self._sample_noise(position, sample_shape)
+        time = self._step_time(time)
+        time = time.expand_as(position[..., -1:])
+        return torch.cat([position, time], dim=-1), logp
 
-    def _deceleration(self, state):
+    def _deceleration(self, position):
         decay = torch.from_numpy(self._deceleration_decay)
         center = torch.from_numpy(self._deceleration_center)
-        # Consider states as row vectors
+        # Consider positions as row vectors
         # Resulting difference is matrix with diff vectors as rows
         # Calculate the norm of each row
-        distance = torch.norm(state.unsqueeze(-2) - center, dim=-1)
+        distance = torch.norm(position.unsqueeze(-2) - center, dim=-1)
         # distance is a vector with distances to each center
         # Calculate the product of all corresponding decelerations
         deceleration = torch.prod(
@@ -124,15 +124,24 @@ class NavigationEnv(gym.Env):
         sample = dist.rsample(sample_shape=sample_shape)
         return sample, dist.log_prob(sample.detach())
 
+    def _step_time(self, time):
+        timestep = torch.round(self._horizon * time)
+        return (timestep + 1) / self._horizon
+
     def reward_fn(self, state, action, next_state):
         # pylint: disable=unused-argument,missing-docstring
-        next_state = next_state[..., :2]
+        position, _ = self._unpack_state(next_state)
         goal = torch.from_numpy(self._end)
-        return torch.norm(next_state - goal, dim=-1).neg()
+        return torch.norm(position - goal, dim=-1).neg()
 
     def _terminal(self):
-        pos, time = self._state[:2], self._state[2]
-        return np.allclose(pos, self._end, atol=1e-1) or np.allclose(time, 1.0)
+        position, time = self._unpack_state(self._state)
+        return np.allclose(position, self._end, atol=1e-1) or time.item() >= 1.0
+
+    @staticmethod
+    def _unpack_state(state):
+        position, time = state[..., :-1], state[..., -1:]
+        return position, time
 
     def render(self, mode="human"):
         pass