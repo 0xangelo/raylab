396f1a1622326c9cfdd55d623afab76af88c85f2
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-01 19:59:25 -0300

    feat(utils): add NumpyBuffer
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/utils/replay_buffer.py
+++ raylab/utils/replay_buffer.py
@@ -4,6 +4,7 @@ import sys
 from dataclasses import dataclass
 
 import numpy as np
+from gym.spaces import Space
 from ray.rllib import SampleBatch
 from ray.rllib.optimizers.replay_buffer import ReplayBuffer as _ReplayBuffer
 from ray.rllib.utils import override
@@ -91,3 +92,86 @@ class ReplayBuffer(_ReplayBuffer):
     def all_samples(self):
         """Return all transitions in buffer as a SampleBatch."""
         return self.sample_with_idxes(range(len(self)))
+
+
+class NumpyReplayBuffer:
+    """Replay buffer using a list of tuples as the internal data structure.
+
+    Returns a SampleBatch object when queried for samples.
+
+    Arguments:
+        obs_space: observation space
+        action_space: action space
+        size: max number of transitions to store in the buffer. When the buffer
+            overflows the old memories are dropped.
+    """
+
+    def __init__(self, obs_space: Space, action_space: Space, size: int):
+        # pylint:disable=too-many-arguments
+        self._maxsize = size
+        self.fields = (
+            ReplayField(
+                SampleBatch.CUR_OBS, shape=obs_space.shape, dtype=obs_space.dtype
+            ),
+            ReplayField(
+                SampleBatch.ACTIONS, shape=action_space.shape, dtype=action_space.dtype
+            ),
+            ReplayField(SampleBatch.REWARDS, shape=(), dtype=np.float32),
+            ReplayField(
+                SampleBatch.NEXT_OBS, shape=obs_space.shape, dtype=obs_space.dtype
+            ),
+            ReplayField(SampleBatch.DONES, shape=(), dtype=np.bool),
+        )
+        self._storage = {}
+        self._build_buffers(*self.fields)
+        self._next_idx = 0
+        self._curr_size = 0
+        self._rng = np.random.default_rng()
+
+    def __len__(self) -> int:
+        return self._curr_size
+
+    def _build_buffers(self, *fields: ReplayField):
+        storage = self._storage
+        size = self._maxsize
+        for field in fields:
+            storage[field.name] = np.empty((size,) + field.shape, dtype=field.dtype)
+
+    def seed(self, seed: int = None):
+        """Seed the random number generator for sampling minibatches."""
+        self._rng = np.random.default_rng(seed)
+
+    def add_fields(self, *fields: ReplayField):
+        """Add fields to the replay buffer and build the corresponding storage."""
+        self.fields = self.fields + fields
+        self._build_buffers(*fields)
+
+    def add(self, row: dict):  # pylint: disable=arguments-differ
+        """Add a row from a SampleBatch to storage.
+
+        Arguments:
+            row: sample batch row as returned by SampleBatch.rows().
+                Must have the same keys as the field names in the buffer.
+        """
+        for field in self.fields:
+            self._storage[field.name][self._next_idx] = row[field.name]
+
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+        self._curr_size += 1 if self._curr_size < self._maxsize else 0
+
+    def sample(self, batch_size: int) -> SampleBatch:
+        """Get a SampleBatch of transitions uniformly sampled with replacement."""
+        return self.sample_with_idxes(self.sample_idxes(batch_size))
+
+    def sample_idxes(self, batch_size: int) -> np.ndarray:
+        """Get random transition indexes uniformly sampled with replacement."""
+        return self._rng.integers(self._curr_size, size=batch_size)
+
+    def sample_with_idxes(self, idxes: np.ndarray) -> SampleBatch:
+        """Get a SampleBatch of transitions corresponding with the given indexes."""
+        batch = {k: self._storage[k][idxes] for k in (f.name for f in self.fields)}
+        return SampleBatch(batch)
+
+    def all_samples(self) -> SampleBatch:
+        """Get all stored transitions as a SampleBatch."""
+        return SampleBatch({k: self._storage[k] for k in (f.name for f in self.fields)})
