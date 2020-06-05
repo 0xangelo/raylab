13b10c24cbfd49bcd2ed1815311c7bbcd049df9a
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-05 17:52:27 -0300

    feat(policy): add ModelSamplingMixin
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/policy/__init__.py
+++ raylab/policy/__init__.py
@@ -1,12 +1,14 @@
 """Collection of custom RLlib Policy classes."""
 
 from .kl_coeff_mixin import AdaptiveKLCoeffMixin
+from .model_based import ModelSamplingMixin
 from .model_based import ModelTrainingMixin
 from .model_based_mixin import ModelBasedMixin
 from .target_networks_mixin import TargetNetworksMixin
 from .torch_policy import TorchPolicy
 
 __all__ = [
+    "ModelSamplingMixin",
     "ModelTrainingMixin",
     "AdaptiveKLCoeffMixin",
     "ModelBasedMixin",
--- raylab/policy/model_based/__init__.py
+++ raylab/policy/model_based/__init__.py
@@ -1,7 +1,9 @@
 """Mixins for model-based policies."""
 
+from .sampling_mixin import ModelSamplingMixin
 from .training_mixin import ModelTrainingMixin
 
 __all__ = [
     "ModelTrainingMixin",
+    "ModelSamplingMixin",
 ]
