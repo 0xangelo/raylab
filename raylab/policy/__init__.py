554b3e511eee6b4d2a49b6250bf40a6cd9321071
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-06 10:16:09 -0300

    chore(policy): remove ModelBasedMixin
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/policy/__init__.py
+++ raylab/policy/__init__.py
@@ -4,16 +4,14 @@ from .kl_coeff_mixin import AdaptiveKLCoeffMixin
 from .model_based import EnvFnMixin
 from .model_based import ModelSamplingMixin
 from .model_based import ModelTrainingMixin
-from .model_based_mixin import ModelBasedMixin
 from .target_networks_mixin import TargetNetworksMixin
 from .torch_policy import TorchPolicy
 
 __all__ = [
+    "AdaptiveKLCoeffMixin",
+    "EnvFnMixin",
     "ModelSamplingMixin",
     "ModelTrainingMixin",
-    "EnvFnMixin",
-    "AdaptiveKLCoeffMixin",
-    "ModelBasedMixin",
     "TargetNetworksMixin",
     "TorchPolicy",
 ]
