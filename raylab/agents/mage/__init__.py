f5513b999993f25318e9a1c7f13cd9fd8999f676
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-12 19:46:31 -0300

    feat(agents): add MAGETorchPolicy
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ raylab/agents/mage/__init__.py
@@ -0,0 +1,5 @@
+"""Model-based Action-Value-Gradient Estimator."""
+
+from .policy import MAGETorchPolicy
+from .trainer import DEFAULT_CONFIG
+from .trainer import MAGETrainer
---
+++ raylab/agents/mage/trainer.py
@@ -0,0 +1,47 @@
+"""Trainer and default config for MAGE."""
+from raylab.agents.model_based import ModelBasedTrainer
+from raylab.agents.model_based import with_base_config
+from raylab.policy.model_based.training_mixin import TrainingSpec
+
+from .policy import MAGETorchPolicy
+
+DEFAULT_CONFIG = with_base_config(
+    {
+        # Clipped Double Q-Learning: use the minimun of two target Q functions
+        # as the next action-value in the target for fitted Q iteration
+        "clipped_double_q": True,
+        # === MAGETorchPolicy ===
+        # PyTorch optimizers to use
+        "torch_optimizer": {
+            "models": {"type": "Adam"},
+            "actor": {"type": "Adam"},
+            "critics": {"type": "Adam"},
+        },
+        # Interpolation factor in polyak averaging for target networks.
+        "polyak": 0.995,
+        "model_training": TrainingSpec().to_dict(),
+        "module": {"type": "MAPOModule", "model": {"ensemble_size": 1}},
+        # === Exploration Settings ===
+        # Default exploration behavior, iff `explore`=None is passed into
+        # compute_action(s).
+        # Set to False for no exploration behavior (e.g., for evaluation).
+        "explore": True,
+        # Provide a dict specifying the Exploration object's config.
+        "exploration_config": {
+            "type": "raylab.utils.exploration.GaussianNoise",
+            "noise_stddev": 0.3,
+            "pure_exploration_steps": 1000,
+        },
+        # === Evaluation ===
+        # Extra arguments to pass to evaluation workers.
+        "evaluation_config": {"explore": False},
+    }
+)
+
+
+class MAGETrainer(ModelBasedTrainer):
+    """Single agent trainer for MAGE."""
+
+    _name = "MAGE"
+    _default_config = DEFAULT_CONFIG
+    _policy = MAGETorchPolicy
