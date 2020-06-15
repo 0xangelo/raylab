b8449f7bc69ab0ef42dfa9890d870ad60a0b5388
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-15 08:31:46 -0300

    test(agents): add delayed policy updates
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/agents/sop/test_policy.py
+++ tests/agents/sop/test_policy.py
@@ -3,6 +3,7 @@ import pytest
 import torch
 
 from raylab.agents.sop import SOPTorchPolicy
+from raylab.utils.debug import fake_batch
 
 
 @pytest.fixture(params=(True, False))
@@ -12,7 +13,7 @@ def clipped_double_q(request):
 
 @pytest.fixture
 def config(clipped_double_q):
-    return {"clipped_double_q": clipped_double_q}
+    return {"clipped_double_q": clipped_double_q, "policy_delay": 2}
 
 
 @pytest.fixture
@@ -30,3 +31,21 @@ def test_target_params_update(policy):
         param.data.add_(torch.ones_like(param))
     policy.update_targets("critics", "target_critics")
     assert all(not torch.allclose(p, q) for p, q in zip(target_params, old_params))
+
+
+@pytest.fixture
+def samples(obs_space, action_space):
+    return fake_batch(obs_space, action_space, batch_size=256)
+
+
+def test_delayed_policy_update(policy, samples):
+    actor = policy.module.actor
+    params = [p.clone() for p in actor.parameters()]
+    _ = policy.learn_on_batch(samples)
+
+    assert all(torch.allclose(new, old) for new, old in zip(actor.parameters(), params))
+
+    _ = policy.learn_on_batch(samples)
+    assert all(
+        not torch.allclose(new, old) for new, old in zip(actor.parameters(), params)
+    )
