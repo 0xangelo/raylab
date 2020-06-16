dcc7a1b6bbd1d09846011abaa334ec965973564f
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-16 10:13:32 -0300

    chore: bump version patch
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/__init__.py
+++ raylab/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ângelo Gregório Lovatto"""
 __email__ = "angelolovatto@gmail.com"
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 
 
 def register_all_agents():
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=TEST_REQUIREMENTS,
     url="https://github.com/angelolovatto/raylab",
-    version="0.7.3",
+    version="0.7.4",
     zip_safe=False,
 )
