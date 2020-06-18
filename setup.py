40e7d5a1ca8344a2cf2748be30ff16085e52882f
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-18 08:07:31 -0300

    chore: bump version patch
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite="tests",
     tests_require=TEST_REQUIREMENTS,
     url="https://github.com/angelolovatto/raylab",
-    version="0.7.5",
+    version="0.7.6",
     zip_safe=False,
 )
