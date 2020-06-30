880e02e24a0cce156f8f329c1e7c9cda788ff76d
Ângelo Lovatto <angelolovatto@gmail.com>
2020-06-30 16:30:05 -0300

    refactor(tests): match test directory structure with package
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ tests/raylab/conftest.py
@@ -0,0 +1,38 @@
+# pylint: disable=missing-docstring,redefined-outer-name,protected-access
+import gym.spaces as spaces
+import pytest
+
+
+@pytest.fixture(scope="module", params=((1,), (4,)), ids=("Obs1Dim", "Obs4Dim"))
+def obs_space(request):
+    return spaces.Box(-10, 10, shape=request.param)
+
+
+@pytest.fixture(scope="module", params=((1,), (4,)), ids=("Act1Dim", "Act4Dim"))
+def action_space(request):
+    return spaces.Box(-1, 1, shape=request.param)
+
+
+@pytest.fixture(scope="module")
+def envs():
+    from raylab.envs.registry import ENVS  # pylint:disable=import-outside-toplevel
+
+    return ENVS.copy()
+
+
+@pytest.fixture(
+    params="""
+    MockEnv
+    Navigation
+    Reservoir
+    HVAC
+    MountainCarContinuous-v0
+    """.split()
+)
+def env_name(request):
+    return request.param
+
+
+@pytest.fixture
+def env_creator(envs, env_name):
+    return envs[env_name]
