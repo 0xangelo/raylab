167611b42e229e6fe3ceaccd788232c6f3c1afe9
Ângelo Lovatto <angelolovatto@gmail.com>
2020-07-06 20:10:17 -0300

    test(config): assert RLlib's common configs have help texts
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ tests/raylab/agents/test_config.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-docstring,redefined-outer-name,protected-access
+import copy
+
+import pytest
+
+from raylab.agents.config import recursive_check_info
+
+
+@pytest.fixture()
+def common_config():
+    from ray.rllib.agents.trainer import with_common_config
+
+    return with_common_config({})
+
+
+@pytest.fixture()
+def common_info():
+    from raylab.agents.config import with_rllib_info
+
+    return with_rllib_info({})
+
+
+@pytest.fixture()
+def allow_new_subkey_list():
+    from ray.rllib.agents.trainer import Trainer
+
+    return copy.deepcopy(Trainer._allow_unknown_subkeys)
+
+
+def test_rllib_info(common_config, common_info, allow_new_subkey_list):
+    recursive_check_info(common_config, common_info, allow_new_subkey_list)
