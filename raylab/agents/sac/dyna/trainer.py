0bf7a80aa5aafccea3aa6ea8637f3cb5e0a37121
Ângelo Lovatto <angelolovatto@gmail.com>
2020-07-13 11:43:05 -0300

    fix(dyna-sac): register default model training config
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/agents/sac/dyna/trainer.py
+++ raylab/agents/sac/dyna/trainer.py
@@ -2,11 +2,13 @@
 from raylab.agents import trainer
 from raylab.agents.model_based import ModelBasedTrainer
 from raylab.agents.sac.trainer import sac_config
+from raylab.policy.model_based.training_mixin import TrainingSpec
 
 from .policy import DynaSACTorchPolicy
 
 
-@trainer.config("module", {"type": "ModelBasedSAC"})
+@trainer.config("module/type", "ModelBasedSAC")
+@trainer.config("model_training", TrainingSpec().to_dict(), info=TrainingSpec.__doc__)
 @trainer.config("exploration_config/pure_exploration_steps", 1000)
 @trainer.config("evaluation_config/explore", False, override=True)
 @sac_config
