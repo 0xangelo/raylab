e6de06c321285b8bedc8464f59dbceab6861669d
Ângelo Lovatto <angelolovatto@gmail.com>
2020-07-17 12:06:16 -0300

    feat(agents): add base SOP config decorator
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/agents/mage/trainer.py
+++ raylab/agents/mage/trainer.py
@@ -1,51 +1,20 @@
 """Trainer and default config for MAGE."""
 from raylab.agents import trainer
 from raylab.agents.model_based import ModelBasedTrainer
-from raylab.policy.model_based.training_mixin import DataloaderSpec
+from raylab.agents.sop.trainer import sop_config
 from raylab.policy.model_based.training_mixin import TrainingSpec
 
 from .policy import MAGETorchPolicy
 
-MODEL_TRAINING_SPEC = TrainingSpec(
-    dataloader=DataloaderSpec(batch_size=256, replacement=True),
-    max_epochs=None,
-    max_grad_steps=120,
-    max_time=None,
-    patience_epochs=None,
-    improvement_threshold=None,
-).to_dict()
-DEFAULT_MODULE = {"type": "ModelBasedDDPG", "critic": {"double_q": True}}
-TORCH_OPTIMIZERS = {
-    "models": {"type": "Adam"},
-    "actor": {"type": "Adam"},
-    "critics": {"type": "Adam"},
-}
-EXPLORATION_CONFIG = {
-    "type": "raylab.utils.exploration.GaussianNoise",
-    "noise_stddev": 0.3,
-    "pure_exploration_steps": 1000,
-}
-
 
 @trainer.config("lambda", 0.05, info="TD error regularization for MAGE loss")
+@trainer.config("model_training", TrainingSpec().to_dict(), info=TrainingSpec.__doc__)
+@sop_config
+@trainer.config("module/type", "ModelBasedDDPG")
+@trainer.config("torch_optimizer/models", {"type": "Adam"})
 @trainer.config(
-    "polyak",
-    0.995,
-    info="Interpolation factor in polyak averaging for target networks.",
+    "exploration_config/type", "raylab.utils.exploration.GaussianNoise", override=True
 )
-@trainer.config(
-    "policy_delay",
-    1,
-    info="Update policy every this number of calls to `learn_on_batch`",
-)
-@trainer.config(
-    "model_training",
-    MODEL_TRAINING_SPEC,
-    info="See raylab.policy.model_based.ModelTrainingMixin",
-)
-@trainer.config("module", DEFAULT_MODULE, override=True)
-@trainer.config("torch_optimizer", TORCH_OPTIMIZERS, override=True)
-@trainer.config("exploration_config", EXPLORATION_CONFIG, override=True)
 @trainer.config("holdout_ratio", 0, override=True)
 @trainer.config("max_holdout", 0, override=True)
 @trainer.config("evaluation_config/explore", False, override=True)
--- raylab/agents/sop/trainer.py
+++ raylab/agents/sop/trainer.py
@@ -5,52 +5,56 @@ from raylab.agents.off_policy import OffPolicyTrainer
 from .policy import SOPTorchPolicy
 
 
-DEFAULT_MODULE = {
-    "type": "DDPG",
-    "actor": {"separate_behavior": True},
-    "critic": {"double_q": True},
-}
+def sop_config(cls: type) -> type:
+    """Add configurations for Streamlined Off-Policy-based agents."""
+    for config_setter in [
+        trainer.config(
+            "dpg_loss",
+            "default",
+            info="""\
+            Type of Deterministic Policy Gradient to use.
+
+            'default' backpropagates Q-value gradients through the critic network.
+
+            'acme' uses Acme's implementation which recovers DPG via a MSE loss between
+            the actor's action and the action + Q-value gradient. Allows monitoring the
+            magnitude of the action-value gradient.""",
+        ),
+        trainer.config(
+            "dqda_clipping",
+            None,
+            info="""\
+            Optional value by which to clip the action gradients. Only used with
+            dpg_loss='acme'.""",
+        ),
+        trainer.config(
+            "clip_dqda_norm",
+            False,
+            info="""\
+            Whether to clip action grads by norm or value. Only used with
+            dpg_loss='acme'.""",
+        ),
+        trainer.config("torch_optimizer/actor", {"type": "Adam", "lr": 1e-3}),
+        trainer.config("torch_optimizer/critics", {"type": "Adam", "lr": 1e-3}),
+        trainer.config(
+            "polyak",
+            0.995,
+            info="Interpolation factor in polyak averaging for target networks.",
+        ),
+        trainer.config(
+            "policy_delay",
+            1,
+            info="Update policy every this number of calls to `learn_on_batch`",
+        ),
+    ]:
+        cls = config_setter(cls)
+
+    return cls
 
 
-@trainer.config(
-    "dpg_loss",
-    "default",
-    info="""\
-    Type of Deterministic Policy Gradient to use.
-
-    'default' backpropagates Q-value gradients through the critic network.
-
-    'acme' uses Acme's implementation which recovers DPG via a MSE loss between
-    the actor's action and the action + Q-value gradient. Allows monitoring the
-    magnitude of the action-value gradient.""",
-)
-@trainer.config(
-    "dqda_clipping",
-    None,
-    info="""\
-    Optional value by which to clip the action gradients. Only used with
-    dpg_loss='acme'.""",
-)
-@trainer.config(
-    "clip_dqda_norm",
-    False,
-    info="""\
-    Whether to clip action grads by norm or value. Only used with
-    dpg_loss='acme'.""",
-)
-@trainer.config("torch_optimizer/actor", {"type": "Adam", "lr": 1e-3})
-@trainer.config("torch_optimizer/critics", {"type": "Adam", "lr": 1e-3})
-@trainer.config(
-    "polyak",
-    0.995,
-    info="Interpolation factor in polyak averaging for target networks.",
-)
-@trainer.config(
-    "policy_delay",
-    1,
-    info="Update policy every this number of calls to `learn_on_batch`",
-)
-@trainer.config("module", DEFAULT_MODULE, override=True)
+@sop_config
+@trainer.config("module/type", "DDPG")
+@trainer.config("module/actor/separate_behavior", True)
 @trainer.config(
     "exploration_config/type", "raylab.utils.exploration.ParameterNoise", override=True
 )
