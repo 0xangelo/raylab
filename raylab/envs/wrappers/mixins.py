87923b315c34d31a2e3ebf7d62c623f798654f44
Ângelo Lovatto <angelolovatto@gmail.com>
2020-07-25 07:22:25 -0300

    feat(envs): wrap env functions in IrrelevantRedundantMixin
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/envs/wrappers/mixins.py
+++ raylab/envs/wrappers/mixins.py
@@ -1,8 +1,13 @@
 """Mixins for Gym environment wrappers."""
+from abc import ABC
+from abc import abstractmethod
 from typing import List
 from typing import Optional
 
 import gym.utils.seeding as seeding
+import numpy as np
+
+from .utils import ignore_rightmost_variables
 
 
 class RNGMixin:
@@ -25,3 +30,44 @@ class RNGMixin:
         seeds = super().seed(seed) or []
         self.np_random, seed_ = seeding.np_random(seed)
         return seeds + [seed_]
+
+
+class IrrelevantRedundantMixin(ABC):
+    """Common irrelevant/redundant observation wrapper interface."""
+
+    @property
+    @abstractmethod
+    def added_size(self):
+        """Number of right-most irrelevant/redundant state dimensions."""
+
+    def observation(self, observation: np.ndarray) -> np.ndarray:
+        """Concatenate irrelevant/redundant variables to the observation."""
+        irrelevant_or_redundant = self._added_vars(observation)
+        observation = np.concatenate([observation, irrelevant_or_redundant])
+        return observation.astype(self.observation_space.dtype)
+
+    @staticmethod
+    def wrap_env_function(func: callable, size: int) -> callable:
+        """Wrap base env reward/termination function to ignore added variables.
+
+        Args:
+            func: Callable for reward/termination function
+            size: Number of irrelevant/redundant variables
+        """
+        return ignore_rightmost_variables(func, size)
+
+    @abstractmethod
+    def _added_vars(self, observation: np.ndarray) -> np.ndarray:
+        pass
+
+    def _set_reward_if_possible(self):
+        if hasattr(self.env, "reward_fn"):
+            self.reward_fn = ignore_rightmost_variables(
+                self.env.reward_fn, self.added_size
+            )
+
+    def _set_termination_if_possible(self):
+        if hasattr(self.env, "termination_fn"):
+            self.reward_fn = ignore_rightmost_variables(
+                self.env.termination_fn, self.added_size
+            )
