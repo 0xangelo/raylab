d214e13dffe621e3ad7e96f7f84255c6b9dc5f40
Ângelo Lovatto <angelolovatto@gmail.com>
2020-09-09 08:00:17 -0300

    refactor(policy): rename model-based mixins
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/policy/__init__.py
+++ raylab/policy/__init__.py
@@ -1,16 +1,5 @@
 """Collection of custom RLlib Policy classes."""
 from .kl_coeff_mixin import AdaptiveKLCoeffMixin
-from .model_based import EnvFnMixin
-from .model_based import ModelSamplingMixin
-from .model_based import ModelTrainingMixin
+from .model_based import *
 from .optimizer_collection import OptimizerCollection
 from .torch_policy import TorchPolicy
-
-__all__ = [
-    "AdaptiveKLCoeffMixin",
-    "EnvFnMixin",
-    "ModelSamplingMixin",
-    "ModelTrainingMixin",
-    "TorchPolicy",
-    "OptimizerCollection",
-]
