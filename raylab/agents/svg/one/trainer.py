c23bfe15da83de630d42476a63a1447f0bc4e183
Ângelo Lovatto <angelolovatto@gmail.com>
2020-09-14 15:03:14 -0300

    chore(agents): port SVG(1) to new trainer/policy standard
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/agents/svg/one/trainer.py
+++ raylab/agents/svg/one/trainer.py
@@ -1,44 +1,25 @@
 """Trainer and configuration for SVG(1)."""
-from ray.rllib import SampleBatch
 from ray.rllib.utils import override
 
 from raylab.agents.model_based import set_policy_with_env_fn
-from raylab.agents.off_policy import OffPolicyTrainer
-from raylab.options import configure
-from raylab.options import option
-from raylab.utils.replay_buffer import ReplayField
+from raylab.agents.off_policy import SimpleOffPolicy
 
 from .policy import SVGOneTorchPolicy
 
 
-@configure
-@option(
-    "replay_kl",
-    True,
-    help="""
-    Whether to penalize KL divergence with the current policy or past policies
-    that generated the replay pool.
-    """,
-)
-@option("evaluation_config/explore", False)
-class SVGOneTrainer(OffPolicyTrainer):
+class SVGOneTrainer(SimpleOffPolicy):
     """Single agent trainer for SVG(1)."""
 
-    # pylint:disable=attribute-defined-outside-init
+    # pylint:disable=abstract-method
     _name = "SVG(1)"
-    _policy = SVGOneTorchPolicy
 
-    @override(OffPolicyTrainer)
-    def _init(self, config, env_creator):
-        super()._init(config, env_creator)
+    def get_policy_class(self, _):
+        return SVGOneTorchPolicy
+
+    def optimize_policy_backend(self):
+        pass
+
+    @override(SimpleOffPolicy)
+    def after_init(self):
         set_policy_with_env_fn(self.workers, fn_type="reward")
-
-    @override(OffPolicyTrainer)
-    def build_replay_buffer(self, config):
-        super().build_replay_buffer(config)
-        self.replay.add_fields(ReplayField(SampleBatch.ACTION_LOGP))
-
-    @override(OffPolicyTrainer)
-    def _before_replay_steps(self, policy):
-        if not self.config["replay_kl"]:
-            policy.update_old_policy()
+        super().optimize_policy_backend()
