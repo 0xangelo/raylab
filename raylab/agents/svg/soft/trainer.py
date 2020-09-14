5278e17a490b7f0c828c39d59f3f90ab903e4c49
Ângelo Lovatto <angelolovatto@gmail.com>
2020-09-14 15:13:28 -0300

    chore(agents): port SoftSVG to new trainer/policy standard
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/agents/svg/soft/trainer.py
+++ raylab/agents/svg/soft/trainer.py
@@ -1,31 +1,25 @@
 """Trainer and configuration for SVG(1) with maximum entropy."""
-from ray.rllib import SampleBatch
 from ray.rllib.utils import override
 
 from raylab.agents.model_based import set_policy_with_env_fn
-from raylab.agents.off_policy import OffPolicyTrainer
-from raylab.options import configure
-from raylab.options import option
-from raylab.utils.replay_buffer import ReplayField
+from raylab.agents.off_policy import SimpleOffPolicy
 
 from .policy import SoftSVGTorchPolicy
 
 
-@configure
-@option("evaluation_config/explore", False, override=True)
-class SoftSVGTrainer(OffPolicyTrainer):
+class SoftSVGTrainer(SimpleOffPolicy):
     """Single agent trainer for SoftSVG."""
 
     # pylint:disable=attribute-defined-outside-init
     _name = "SoftSVG"
-    _policy = SoftSVGTorchPolicy
 
-    @override(OffPolicyTrainer)
-    def _init(self, config, env_creator):
-        super()._init(config, env_creator)
+    def get_policy_class(self, _):
+        return SoftSVGTorchPolicy
+
+    def optimize_policy_backend(self):
+        pass
+
+    @override(SimpleOffPolicy)
+    def after_init(self):
         set_policy_with_env_fn(self.workers, fn_type="reward")
-
-    @override(OffPolicyTrainer)
-    def build_replay_buffer(self, config):
-        super().build_replay_buffer(config)
-        self.replay.add_fields(ReplayField(SampleBatch.ACTION_LOGP))
+        super().optimize_policy_backend()
