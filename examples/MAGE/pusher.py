3bf34d6358226ae6ac3dac2d6f7422b6cc7c1c5e
Ângelo Lovatto <angelolovatto@gmail.com>
2020-09-22 18:02:05 -0300

    feat(examples): add MAGE on Pusher-v2
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ examples/MAGE/pusher.py
@@ -0,0 +1,42 @@
+LEARNING_STARTS = 1000
+
+
+def policy_config() -> dict:
+    return {
+        "buffer_size": int(1e5),
+        "exploration_config": {"pure_exploration_steps": LEARNING_STARTS},
+    }
+
+
+def env_config(env: str = "Pusher-v2") -> dict:
+    return {
+        "env": env,
+        "env_config": {
+            "time_aware": True,
+            "max_episode_steps": 100,
+            "single_precision": True,
+        },
+    }
+
+
+def wandb_config() -> dict:
+    return {"wandb": {"project": "baselines", "entity": "angelovtt"}}
+
+
+def trainer_config() -> dict:
+    return {
+        "timesteps_per_iteration": 1000,
+        "rollout_fragment_length": 1,
+        "evaluation_interval": 1,
+        "evaluation_num_episodes": 10,
+        "evaluation_config": {"explore": False},
+        "learning_starts": LEARNING_STARTS,
+    }
+
+
+def get_config() -> dict:
+    cnf = {"policy": policy_config()}
+    cnf.update(env_config())
+    cnf.update(trainer_config())
+    cnf.update(wandb_config())
+    return cnf
