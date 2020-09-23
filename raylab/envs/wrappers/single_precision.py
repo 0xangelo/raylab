fix(envs): avoid conflicting method inheritance in single precision
Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

raylab/envs/wrappers/single_precision.py
@@ -4,7 +4,7 @@ import numpy as np
 from gym.spaces import Box
 
 
-class SinglePrecision(gym.ObservationWrapper, gym.ActionWrapper):
+class SinglePrecision(gym.Wrapper):
     """Ensures environment outputs are single-precision floats.
 
     Only compatible with continuous state-action environments.
@@ -27,6 +27,15 @@ class SinglePrecision(gym.ObservationWrapper, gym.ActionWrapper):
             low=env.action_space.low.astype(np.float32),
             high=env.action_space.high.astype(np.float32),
         )
+
+    def reset(self, **kwargs):
+        observation = self.env.reset(**kwargs)
+        return self.observation(observation)
+
+    def step(self, action):
+        action = self.action(action)
+        observation, reward, done, info = self.env.step(action)
+        return self.observation(observation), reward, done, info
 
     def observation(self, observation: np.ndarray) -> np.ndarray:
         return observation.astype(self.observation_space.dtype, copy=False)