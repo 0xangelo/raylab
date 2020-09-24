d6c2b185ad33575e9c287c0a12e582f4e1bad895
Ângelo Lovatto <angelolovatto@gmail.com>
2020-09-24 12:07:33 -0300

    test(policy): update expected number of epochs
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/raylab/policy/model_based/test_policy.py
+++ tests/raylab/policy/model_based/test_policy.py
@@ -1,3 +1,5 @@
+import itertools
+
 import pytest
 from ray.rllib.evaluation.metrics import get_learner_stats
 
@@ -16,9 +18,10 @@ def policy_cls(base_policy_cls):
             super().__init__(*args, **kwargs)
             self.build_replay_buffer()
             self.build_timers()
+            self._epoch_seq = itertools.count()
 
         def train_dynamics_model(self, warmup: bool = False):
-            return [], {"model_epochs": 0}
+            return [], {"model_epochs": next(self._epoch_seq) + 1}
 
         def improve_policy(self, batch) -> dict:
             return {"improved": True}
@@ -47,12 +50,11 @@ def policy(policy_cls, config):
 
 
 def test_update_interval(policy, model_update_interval, samples):
+    expected = 0
+
     for i in range(1, model_update_interval * 10 + 1):
         info = policy.learn_on_batch(samples)
         info = get_learner_stats(info)
         assert policy._learn_calls == i
-
-        if i == 1 or i % model_update_interval == 0:
-            assert "model_epochs" in info
-        else:
-            assert "model_epochs" not in info
+        expected += 1 if (i % model_update_interval == 0 or i == 1) else 0
+        assert info["model_epochs"] == expected
