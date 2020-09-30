e777a93c59c7374f8e4929734df7f1428168d343
Ângelo Lovatto <angelolovatto@gmail.com>
2020-09-30 15:18:36 -0300

    Revert "refactor(utils): add storage class for replays"
    This reverts commit 7821a90b2fb2873466b37b7b1d6eb9c0682f9c6b.

--- raylab/utils/replay_buffer.py
+++ raylab/utils/replay_buffer.py
@@ -1,7 +1,6 @@
 """Custom Replay Buffers based on RLlibs's implementation."""
 from dataclasses import dataclass
 from typing import Dict
-from typing import Mapping
 from typing import Optional
 from typing import Tuple
 from typing import Union
@@ -20,86 +19,8 @@ class ReplayField:
     dtype: np.dtype = np.float32
 
 
-class Storage:
-    """Replay storage as a dict of ndarrays.
-
-    Args:
-        size: Maximum length of arrays to store.
-
-    Attributes:
-        maxsize: Maximum length of stored arrays
-        next_idx: Next starting index for new arrays
-        fields (:obj:`tuple` of :obj:`ReplayField`): storage fields
-            specification
-    """
-
-    def __init__(self, size: int):
-        self.maxsize = size
-        self.fields = ()
-        self.buffer = {}
-        self.next_idx = 0
-        self.curr_size = 0
-
-    def __len__(self) -> int:
-        return self.curr_size
-
-    def __getitem__(
-        self, index: Union[int, np.ndarray, slice]
-    ) -> Dict[str, np.ndarray]:
-        return {f.name: self.buffer[f.name][index] for f in self.fields}
-
-    def column(self, name: str) -> np.ndarray:
-        """Returns a full array of one of the buffers."""
-        return self.buffer[name][: len(self)]
-
-    def add_fields(self, *fields: ReplayField):
-        """Add fields and build the corresponding buffers."""
-        new_names = {f.name for f in fields}
-        assert len(new_names) == len(fields), "Field names must be unique"
-
-        conflicts = new_names.intersection({f.name for f in self.fields})
-        assert not conflicts, f"{conflicts} are already in buffer"
-
-        self.fields = self.fields + fields
-        self._build_buffers(*fields)
-
-    def _build_buffers(self, *fields: ReplayField):
-        size = self.maxsize
-        for field in fields:
-            self.buffer[field.name] = np.empty((size,) + field.shape, dtype=field.dtype)
-
-    def add(self, arrs: Mapping[str, np.ndarray]):
-        """Add a collection of arrays to storage.
-
-        Args:
-            arrs: A mapping from strings to arrays. Must have all the keys
-                specificied by this instance's field names.
-        """
-        keys = [f.name for f in self.fields]
-        count = len(arrs[keys[0]])
-        assert all(len(arrs[k]) == count for k in keys)
-
-        if count >= self.maxsize:
-            for key in keys:
-                self.buffer[key] = arrs[key][-self.maxsize :]
-            self.next_idx = 0
-        else:
-            start = self.next_idx
-            end = (start + count) % self.maxsize
-            if start <= end:
-                for key in keys:
-                    self.buffer[key][start:end] = arrs[key]
-            else:
-                tailcount = self.maxsize - start
-                for key in keys:
-                    self.buffer[key][start:] = arrs[key][:tailcount]
-                    self.buffer[key][:end] = arrs[key][-tailcount:]
-
-        self.curr_size = min(self.curr_size + count, self.maxsize)
-
-
 class NumpyReplayBuffer:
-    """Replay buffer as a collection of ndarrays.
+    """Replay buffer as a dict of ndarrays.
 
     Returns a SampleBatch object when queried for samples.
 
@@ -110,15 +31,18 @@ class NumpyReplayBuffer:
             When the bufferoverflows the old memories are dropped.
 
     Attributes:
+        fields (:obj:`tuple` of :obj:`ReplayField`): storage fields
+            specification
         compute_stats: Whether to track mean and stddev for normalizing
             observations
     """
 
+    # pylint:disable=too-many-instance-attributes
     compute_stats: bool = False
 
     def __init__(self, obs_space: Space, action_space: Space, size: int):
-        self._storage = Storage(size)
-        self.add_fields(
+        self._maxsize = size
+        self.fields = (
             ReplayField(
                 SampleBatch.CUR_OBS, shape=obs_space.shape, dtype=obs_space.dtype
             ),
@@ -131,25 +55,37 @@ class NumpyReplayBuffer:
             ),
             ReplayField(SampleBatch.DONES, shape=(), dtype=np.bool),
         )
+        self._storage = {}
+        self._build_buffers(*self.fields)
+        self._next_idx = 0
+        self._curr_size = 0
         self._rng = np.random.default_rng()
         self._obs_stats: Optional[Tuple[np.ndarray, np.ndarray]] = None
 
     def __len__(self) -> int:
-        return len(self._storage)
+        return self._curr_size
 
     def add_fields(self, *fields: ReplayField):
-        """Add fields and build the corresponding storage."""
-        self._storage.add_fields(*fields)
+        """Add fields to the replay buffer and build the corresponding storage."""
+        new_names = {f.name for f in fields}
+        assert len(new_names) == len(fields), "Field names must be unique"
 
-    @property
-    def fields(self):
-        """All registered replay fields in this buffer."""
-        return self._storage.fields
+        conflicts = new_names.intersection({f.name for f in self.fields})
+        assert not conflicts, f"{conflicts} are already in buffer"
+
+        self.fields = self.fields + fields
+        self._build_buffers(*fields)
+
+    def _build_buffers(self, *fields: ReplayField):
+        storage = self._storage
+        size = self._maxsize
+        for field in fields:
+            storage[field.name] = np.empty((size,) + field.shape, dtype=field.dtype)
 
     def __getitem__(
         self, index: Union[int, np.ndarray, slice]
     ) -> Dict[str, np.ndarray]:
-        batch = self._storage[index]
+        batch = {f.name: self._storage[f.name][index] for f in self.fields}
         for key in SampleBatch.CUR_OBS, SampleBatch.NEXT_OBS:
             batch[key] = self.normalize(batch[key])
         return batch
@@ -174,10 +110,10 @@ class NumpyReplayBuffer:
         if len(self) == 0:
             self._obs_stats = (0, 1)
         else:
-            cur_obs = self._storage.column(SampleBatch.CUR_OBS)
+            cur_obs = self._storage[SampleBatch.CUR_OBS][: len(self)]
             mean = np.mean(cur_obs, axis=0)
             std = np.std(cur_obs, axis=0)
-            std = np.where(std < 1e-12, np.ones_like(std), std)
+            std[std < 1e-12] = 1.0
             self._obs_stats = (mean, std)
 
     def seed(self, seed: int = None):
@@ -187,10 +123,47 @@ class NumpyReplayBuffer:
     def add(self, samples: SampleBatch):
         """Add a SampleBatch to storage.
 
+        Optimized to avoid several queries for large sample batches.
+
         Args:
             samples: The sample batch
         """
-        self._storage.add(samples)
+        if samples.count >= self._maxsize:
+            samples = samples.slice(samples.count - self._maxsize, None)
+            end_idx = 0
+            assign = [(slice(0, self._maxsize), samples)]
+        else:
+            start_idx = self._next_idx
+            end_idx = (self._next_idx + samples.count) % self._maxsize
+            if end_idx < start_idx:
+                tailcount = self._maxsize - start_idx
+                assign = [
+                    (slice(start_idx, None), samples.slice(0, tailcount)),
+                    (slice(end_idx), samples.slice(tailcount, None)),
+                ]
+            else:
+                assign = [(slice(start_idx, end_idx), samples)]
+
+        for field in self.fields:
+            for slc, smp in assign:
+                self._storage[field.name][slc] = smp[field.name]
+
+        self._next_idx = end_idx
+        self._curr_size = min(self._curr_size + samples.count, self._maxsize)
+        self._obs_stats = None
+
+    def add_row(self, row: dict):
+        """Add a row from a SampleBatch to storage.
+
+        Args:
+            row: sample batch row as returned by SampleBatch.rows().
+                Must have the same keys as the field names in the buffer.
+        """
+        for field in self.fields:
+            self._storage[field.name][self._next_idx] = row[field.name]
+
+        self._next_idx = (self._next_idx + 1) % self._maxsize
+        self._curr_size += 1 if self._curr_size < self._maxsize else 0
         self._obs_stats = None
 
     def sample(self, batch_size: int) -> SampleBatch:
@@ -199,7 +172,7 @@ class NumpyReplayBuffer:
 
     def sample_idxes(self, batch_size: int) -> np.ndarray:
         """Get random transition indexes uniformly sampled with replacement."""
-        return self._rng.integers(len(self), size=batch_size)
+        return self._rng.integers(self._curr_size, size=batch_size)
 
     def all_samples(self) -> SampleBatch:
         """All stored transitions."""
