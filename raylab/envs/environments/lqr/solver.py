c7b34f378bf3e50946b919f05123ca84ec8e19cc
Ângelo Lovatto <angelolovatto@gmail.com>
2020-10-02 10:55:07 -0300

    fix(envs): make solver scriptable
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- raylab/envs/environments/lqr/solver.py
+++ raylab/envs/environments/lqr/solver.py
@@ -15,6 +15,10 @@ from .types import LQR as System
 from .types import Quadratic
 
 
+Policy = Affine
+Value = Tuple[Tensor, Tensor, Tensor]
+
+
 class LQRSolver(nn.Module):
     """Linear Quadratic Regulator solver."""
 
@@ -22,38 +26,35 @@ class LQRSolver(nn.Module):
 
     def forward(self, LQR: System, T: int):
         # pylint:disable=too-many-arguments,arguments-differ
-        policy: List[Tuple[Tensor, Tensor]] = []
-        value_fn: List[Tuple[Tensor, Tensor, Tensor]] = []
+        policy: List[Policy] = []
+        value_fn: List[Value] = []
 
         Vs = self.final_V(LQR)
-        const = torch.zeros(())
-        value_fn += [Vs + (const,)]
+        value_fn.append(Vs)
 
         for _ in range(T):  # Effectively solving backwards through time
-            Ks, Vs, const_ = self.single_step(LQR, Vs)
-            const += const_
-            policy += [Ks]
-            value_fn += [Vs + (const,)]
+            Ks, Vs = self.single_step(LQR, Vs)
+            policy.insert(0, Ks)
+            value_fn.insert(0, Vs)
 
-        return policy[::-1], value_fn[1:][::-1]
+        return policy, value_fn[:-1]
 
-    def final_V(self, LQR: System):
+    def final_V(self, LQR: System) -> Value:
         _, _, C, c = LQR
         state_size = self.state_size(LQR)
 
-        Vs = (C[:state_size, :state_size], c[:state_size])
+        Vs = (C[:state_size, :state_size], c[:state_size], torch.zeros(()))
         return Vs
 
-    def single_step(self, LQR: System, Vs: Quadratic):
+    def single_step(self, LQR: System, Vs: Value):
         Qs = self.compute_Q(LQR, Vs)
         Ks = self.compute_K(LQR, Qs)
-        const = self.compute_const(LQR, Vs, Ks)
-        Vs = self.compute_V(LQR, Qs, Ks)
-        return Ks, Vs, const
+        Vs = self.compute_V(LQR, Qs, Ks, Vs)
+        return Ks, Vs
 
-    def compute_Q(self, LQR: System, Vs: Quadratic):
+    def compute_Q(self, LQR: System, Vs: Value):
         F, f, C, c = LQR
-        V, v = Vs
+        V, v, _ = Vs
 
         FV = F.T @ V
         Q = C + FV @ F
@@ -73,7 +74,7 @@ class LQRSolver(nn.Module):
         k = -inv_Q_uu @ q_u
         return K, k
 
-    def compute_V(self, LQR: System, Qs: Quadratic, Ks: Affine):
+    def compute_V(self, LQR: System, Qs: Quadratic, Ks: Affine, Vs: Value):
         # pylint:disable=too-many-locals
         state_size = self.state_size(LQR)
         Q, q = Qs
@@ -89,24 +90,27 @@ class LQRSolver(nn.Module):
 
         V = Q_xx + Q_xu @ K + K.T @ Q_ux + K_Q_uu @ K
         v = q_x + Q_xu @ k + K.T @ q_u + K_Q_uu @ k
-        return V, v
+        const = self.compute_const(LQR, Vs, Ks)
+        return V, v, const
 
-    def compute_const(self, LQR: System, Vs: Quadratic, Ks: Affine):
+    def compute_const(self, LQR: System, Vs: Value, Ks: Affine):
+        # pylint:disable=too-many-locals
         W_uu, w_u, V_f = self.compute_W(LQR, Vs)
         _, f, _, _ = LQR
-        _, v = Vs
+        _, v, const_0 = Vs
         _, k = Ks
         const1 = 1 / 2 * k.T @ W_uu @ k
         const2 = k.T @ w_u
         const3 = 1 / 2 * f.T @ V_f + f.T @ v
-        const = const1 + const2 + const3
+        const = const_0 + const1 + const2 + const3
         return const.squeeze()
 
-    def compute_W(self, LQR: System, Vs: Quadratic):
+    def compute_W(self, LQR: System, Vs: Value):
+        # pylint:disable=too-many-locals
         state_size = self.state_size(LQR)
         F, f, C, c = LQR
 
-        V, v = Vs
+        V, v, _ = Vs
         V_f = V @ f
         W = C + F.T @ V @ F
         w = c + F.T @ V_f + F.T @ v
