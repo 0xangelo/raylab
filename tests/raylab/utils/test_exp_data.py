f9dbbde74a65a43aee2e7774637e7d9be82f8566
Ângelo Lovatto <angelolovatto@gmail.com>
2021-04-21 19:51:21 -0300

    test(utils): use py.path.local methods in fixture
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/raylab/utils/test_exp_data.py
+++ tests/raylab/utils/test_exp_data.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import json
-import os
 
 import numpy as np
 import pandas as pd
@@ -9,11 +8,6 @@ import pandas.util.testing as pdtest
 import pytest
 
 from raylab.utils.exp_data import ExperimentData, load_exps_data
-
-
-@pytest.fixture
-def directories(tmpdir) -> list[str]:
-    return [str(tmpdir)]
 
 
 @pytest.fixture
@@ -44,16 +38,19 @@ def config_prefix() -> str:
 
 @pytest.fixture(autouse=True)
 def create_experiment_output(
-    directories: list[str],
+    tmpdir,
     progress: pd.DataFrame,
     params: dict,
     progress_prefix: str,
     config_prefix: str,
 ):
-    for directory in directories:
-        progress.to_csv(os.path.join(directory, progress_prefix + ".csv"), index=False)
-        with open(os.path.join(directory, config_prefix + ".json"), "w") as file:
-            json.dump(params, file)
+    progress.to_csv(tmpdir.join(progress_prefix + ".csv"), index=False)
+    tmpdir.join(config_prefix + ".json").write(json.dumps(params))
+
+
+@pytest.fixture
+def directories(tmpdir) -> list[str]:
+    return [str(tmpdir)]
 
 
 def test_load_exps_data(
