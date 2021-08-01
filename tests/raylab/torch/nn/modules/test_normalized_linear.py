55e478b9f337e3c75a56cfc0e40d7e4a6a78f859
Ângelo Lovatto <angelolovatto@gmail.com>
2021-08-01 07:57:33 -0300

    test(torch): test unclipped outputs gradient propagation
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

--- tests/raylab/torch/nn/modules/test_normalized_linear.py
+++ tests/raylab/torch/nn/modules/test_normalized_linear.py
@@ -1,10 +1,11 @@
 import pytest
 import torch
+from torch import Tensor, nn
 
 from raylab.torch.nn import NormalizedLinear
 
 
-@pytest.fixture(params=(1, 2, 4))
+@pytest.fixture(params=(1, 2))
 def dim(request):
     return request.param
 
@@ -19,29 +20,45 @@ def output_dim(dim):
     return dim
 
 
-@pytest.fixture(params=(0.8, 1.0, 1.2))
+@pytest.fixture(params=(0.8, 1.2))
 def beta(request):
     return request.param
 
 
-def test_normalizes_vector(input_dim, output_dim, beta, torch_script):
+@pytest.fixture
+def inputs(input_dim, beta):
+    return torch.rand(10, input_dim) + torch.full((10, input_dim), beta)
+
+
+@pytest.fixture
+def module(input_dim, output_dim, beta, torch_script):
     module = NormalizedLinear(input_dim, output_dim, beta=beta)
     if torch_script:
         module = torch.jit.script(module)
+    return module
 
-    inputs = torch.randn(10, input_dim)
+
+def test_normalizes_vector(module, inputs, output_dim, beta):
     output = module(inputs)
     norms = output.norm(p=1, dim=-1, keepdim=True) / output_dim
     atol = 1e-6
     assert (norms <= (beta + atol)).all()
 
 
-def test_propagates_gradients(input_dim, output_dim, beta, torch_script):
-    module = NormalizedLinear(input_dim, output_dim, beta=beta)
-    if torch_script:
-        module = torch.jit.script(module)
+@pytest.fixture
+def unclipped_inputs(input_dim: int, beta: float):
+    # Try to force input norm to less than beta
+    return torch.full(size=(10, input_dim), fill_value=beta / (input_dim + 1))
 
-    inputs = torch.randn(10, input_dim, requires_grad=True)
+
+def test_unclipped_values_gradient_propagation(
+    module: NormalizedLinear, unclipped_inputs: Tensor
+):
+    # Force underlying linear module to apply identity map
+    nn.init.eye_(module.linear.weight)
+    nn.init.zeros_(module.linear.bias)
+
+    inputs = unclipped_inputs.requires_grad_(True)
     module(inputs).mean().backward()
     assert inputs.grad is not None
     assert (inputs.grad != 0).any()
